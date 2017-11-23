aa37f50eba627ba5965ac9948a81cb4a3ce8a777
Mark Jansen <Markxjansen@gmail.com>
2017-11-23 12:29:17 +0100

    landing page

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 <meta charset="utf-8">
 <head>
   =
-    <div class="row">
+    <div class="container-fluid">
         <div class="col-sm-4"><img src="img/MBtegel.png" alt="MetselBedrijf"></div>
         <div class="col-sm-4"><img src="img/BBtegel.png" alt="BouwBedrijf"></div>
         <div class="col-sm-4"><img src="img/TBtegel.png" alt="Bouwkundig Tekenbureau"></div>
