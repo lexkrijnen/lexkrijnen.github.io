ca1f4d060afcf869b8071814fb7257eeafc6a47e
lexkrijnen <33865169+lexkrijnen@users.noreply.github.com>
2017-11-23 12:19:01 +0100

    commit test90000000001

--- index.php
+++ index.php
@@ -3,6 +3,6 @@
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <meta charset="utf-8">
 <head>
-   <?php print("Welkom bij Wegro"); ?>
+   <?php print("Welkom bij Wegro!"); ?>
 </head>
 </html>
