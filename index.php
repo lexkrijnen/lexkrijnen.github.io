d3698d6071b061478b7d65516658262736f51170
Mark Jansen <Markxjansen@gmail.com>
2017-11-23 12:05:41 +0100

    logo

---
+++ index.php
@@ -0,0 +1,8 @@
+<!DOCTYPE html>
+<html lang="en">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta charset="utf-8">
+<head>
+   <?php print("Welkom bij Wegro"); ?>
+</head>
+</html>
