2e28a40deef6e47f69afa5d2e12eda05603405bf
Mark Jansen <Markxjansen@gmail.com>
2017-11-23 13:47:16 +0100

    landing page

--- index.php
+++ index.php
@@ -27,7 +27,7 @@
     <nav class="navbar navbar-inverse">
         <div class="container-fluid">
             <div class="navbar-header">
-                <a class="navbar-brand" href=""><img class="logo" src="img/BBtegel.png" alt="BouwBedrijf"></a>
+                <a class="navbar-brand" href=""><img class="logo" src="img/BBbanner.png" alt="BouwBedrijf"></a>
             </div>
             <ul class="nav navbar-nav">
                 <li class="active"><a href="#">Welkom</a></li>
