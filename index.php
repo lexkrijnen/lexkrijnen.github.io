12e2b96c4290ff048b725a7c12fedad51a4e9ffc
Mark Jansen <Markxjansen@gmail.com>
2017-11-23 12:41:47 +0100

    landing page

--- index.php
+++ index.php
@@ -1,15 +1,43 @@
-<!DOCTYPE html>
-<html lang="en">
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<meta charset="utf-8">
-<head>
-  =
-    <div class="container-fluid">
-        <div class="col-sm-4"><img src="img/MBtegel.png" alt="MetselBedrijf"></div>
-        <div class="col-sm-4"><img src="img/BBtegel.png" alt="BouwBedrijf"></div>
-        <div class="col-sm-4"><img src="img/TBtegel.png" alt="Bouwkundig Tekenbureau"></div>
+
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+        <meta name="description" content="Pagina beschrijving">
+        <meta name="author" content="Auteur">
+        <link rel="icon" href="images/favicon.ico">
+
+        <title>Welkom bij wegro</title>
+
+        <!-- Bootstrap core CSS -->
+        <link href="css/bootstrap.min.css" rel="stylesheet">
+
+        <!-- Custom styles for this page -->
+        <link href="css/index.css" rel="stylesheet">
+
+        <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+        <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+        <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+        <![endif]-->
+    </head>
+<body>
+<div class="container">
+    <div class="row">
+        <div class="col-sm-4 tegel"><img src="img/MBtegel.png" alt="MetselBedrijf"></div>
+        <div class="col-sm-4 tegel"><img src="img/BBtegel.png" alt="BouwBedrijf"></div>
+        <div class="col-sm-4 tegel"><img src="img/TBtegel.png" alt="Bouwkundig Tekenbureau"></div>
     </div>
+</div><!-- /container -->
 
 
-</head>
+<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+<script src="js/jquery.min.js"></script>
+
+<!-- Bootstrap Framework -->
+<script src="js/bootstrap.min.js"></script>
+</body>
 </html>
