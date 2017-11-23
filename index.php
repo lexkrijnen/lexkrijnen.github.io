981dc0707cf9c8152d53a6081d85bcfc9be7aa92
Mark Jansen <Markxjansen@gmail.com>
2017-11-23 12:22:59 +0100

    logo

--- index.php
+++ index.php
@@ -4,5 +4,12 @@
 <meta charset="utf-8">
 <head>
    <?php print("Welkom bij Wegro"); ?>
+    <div class="row">
+        <div class="col-sm-4"><img src="MBtegel.png" alt="MetselBedrijf"></div>
+        <div class="col-sm-4"><img src="BBtegel.png" alt="BouwBedrijf"></div>
+        <div class="col-sm-4"><img src="TBtegel.png" alt="Bouwkundig Tekenbureau"></div>
+    </div>
+
+
 </head>
 </html>
