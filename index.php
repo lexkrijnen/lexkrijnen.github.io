299a73c9b961752a753d9ec69883756007847c29
Mark Jansen <Markxjansen@gmail.com>
2017-11-23 12:26:00 +0100

    landing page

--- index.php
+++ index.php
@@ -3,11 +3,11 @@
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <meta charset="utf-8">
 <head>
-   <?php print("Welkom bij Wegro"); ?>
+  =
     <div class="row">
-        <div class="col-sm-4"><img src="MBtegel.png" alt="MetselBedrijf"></div>
-        <div class="col-sm-4"><img src="BBtegel.png" alt="BouwBedrijf"></div>
-        <div class="col-sm-4"><img src="TBtegel.png" alt="Bouwkundig Tekenbureau"></div>
+        <div class="col-sm-4"><img src="img/MBtegel.png" alt="MetselBedrijf"></div>
+        <div class="col-sm-4"><img src="img/BBtegel.png" alt="BouwBedrijf"></div>
+        <div class="col-sm-4"><img src="img/TBtegel.png" alt="Bouwkundig Tekenbureau"></div>
     </div>
 
 
