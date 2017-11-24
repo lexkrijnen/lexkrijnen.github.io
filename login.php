0af58d1cedcc10fa52d8af3b6e4490fedc6cf13b
Mark Jansen <Markxjansen@gmail.com>
2017-11-24 16:11:50 +0100

    landing page

--- login.php
+++ login.php
@@ -19,7 +19,7 @@
 
     <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
     <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-    <body>
+    <body class="center-block">
 <div class="col-md-4">
     <div class="login-panel panel panel-default">
         <div class="panel-heading">
