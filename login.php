0c8bcd6df7101ca3fd1930396b1b90ccc8849fc5
Mark Jansen <Markxjansen@gmail.com>
2017-11-24 16:57:32 +0100

    landing page

--- login.php
+++ login.php
@@ -20,28 +20,40 @@
     <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
     <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
     <body class="midden">
-
 <div class="container">
     <div class="row">
-        <div class="col-md-offset-5 col-md-3">
-            <div class="form-login">
-                <h4>Welcome back.</h4>
-                <input type="text" id="userName" class="form-control input-sm chat-input" placeholder="username" />
-                </br>
-                <input type="text" id="userPassword" class="form-control input-sm chat-input" placeholder="password" />
-                </br>
-                <div class="wrapper">
-            <span class="group-btn">
-                <a href="#" class="btn btn-primary btn-md">login <i class="fa fa-sign-in"></i></a>
-            </span>
-                </div>
-            </div>
-
+<div class="col-md-offset-5 col-md-3">
+    <div class="login-panel panel panel-default">
+        <div class="panel-heading">
+            <h3 class="panel-title">Inloggen bij uw Wegro account</h3>
         </div>
+        <div class="panel-body">
+            <form role="form">
+                <fieldset>
+                    <div class="form-group form-login">
+                        <input class="form-control" placeholder="E-mail" name="email" type="email" autofocus="">
+                    </div>
+                    <div class="form-group">
+                        <input class="form-control" placeholder="Wachtwoord" name="wachtwoord" type="password" value="">
+                    </div>
+                    <div class="checkbox">
+                        <label>
+                            <input name="remember" type="checkbox" value="Remember me">Onthoud mij
+                        </label>
+                    </div>
+                    <a href="javascript:;" class="btn btn-sm btn-success">Login</a>
+                </fieldset>
+            </form>
+        </div>
+</div>
+</div>
     </div>
+</div>
 </div>
 </body>
     <script src="js/jquery.min.js"></script>
     <script src="js/bootstrap.min.js"></script>
     </body>
 </html>
+
+
