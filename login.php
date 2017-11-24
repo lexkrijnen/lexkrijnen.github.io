0aec6f2da8df6f3b74bd0e5babc5fcc494689340
Mark Jansen <Markxjansen@gmail.com>
2017-11-24 16:28:15 +0100

    landing page

--- login.php
+++ login.php
@@ -19,9 +19,10 @@
 
     <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
     <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-    <body class="center-block">
-<div class="login">
+    <body class="login">
+
 <div class="col-md-4">
+    <div class="login">
     <div class="login-panel panel panel-default">
         <div class="panel-heading tegel">
             <h3 class="panel-title">Inloggen bij uw Wegro account</h3>
