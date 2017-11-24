b759f18df5015e11c8298c24bd6a44ddbb0536e8
Mark Jansen <Markxjansen@gmail.com>
2017-11-24 11:40:05 +0100

    landing page

--- index.php
+++ index.php
@@ -27,7 +27,7 @@
                 <a class="navbar-brand" href="#">
                     <img alt="Wegro" src="img/wegrobanner.png" width="75" height="25">
                 </a>
-                <div class="col-xs-11">
+                <div class="col-xs-1 pull-right">
                 <button type="button" class="btn btn-default navbar-btn ">Login</button>
             </div>
             </div>
