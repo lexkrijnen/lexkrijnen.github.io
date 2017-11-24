f489997d8662cb9556b906a1fc787f4b61911a14
Mark Jansen <Markxjansen@gmail.com>
2017-11-24 11:09:10 +0100

    landing page

--- index.php
+++ index.php
@@ -22,14 +22,23 @@
         <![endif]-->
     </head>
     <body>
-    <header>
-        <nav class="navbar navbar-light bg-faded">
-            <a class="navbar-brand" href="#">
-                <img src="/assets/brand/bootstrap-solid.svg" width="30" height="30" class="d-inline-block align-top" alt="p">
-                Bootstrap
-            </a>
+        <nav class="navbar navbar-toggleable-md navbar-light bg-faded">
+            <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <a class="navbar-brand" href="#">Wegro</a>
+
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav mr-auto">
+                    <li class="nav-item active">
+                        <a class="nav-link" href="#">Login <span class="sr-only">(current)</span></a>
+                    </li>
+
+                </ul>
+
+            </div>
         </nav>
-    </header>
+
 <h2>Welkom bij bouwbedrijf wegro.<h2></h2> <br>
     <h2> Maak hier uw keuze om door te gaan.</h2>
     <div class="img">
