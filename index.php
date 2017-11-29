1ef819f408fe357843ada2766acd768e3dbe5eab
JeroenElferink <jeroenelferink13@gmail.com>
2017-11-29 19:30:08 +0100

    1

--- index.php
+++ index.php
@@ -41,7 +41,7 @@
     <div class="row">
         <div class="col-xs-4">
             <div class="col-xs-12">
-                <div <button type="button" class="btn btn-default tegel"><img class="tegelimage" src="img/MBtegel2.png" alt="MetselBedrijf"> </button> </a> </div>
+                <div <a href="metselhomepage.php"> <button type="button" class="btn btn-default tegel"><img class="tegelimage" src="img/MBtegel2.png" alt="MetselBedrijf"> </button> </a>
             </div>
         </div>
         <div class="col-xs-4">
