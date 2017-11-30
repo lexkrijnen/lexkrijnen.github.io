451ee702e32bec82044a7867c2c4805f38e71dbd
JeroenElferink <jeroenelferink13@gmail.com>
2017-11-30 11:31:54 +0100

    Homepages

---
+++ tekenhomepage.php
@@ -0,0 +1,35 @@
+ <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+        <meta name="description" content="Pagina beschrijving">
+        <meta name="author" content="Auteur">
+        <link rel="icon" href="images/favicon.ico">
+
+        <title>Welkom bij wegro</title>
+
+        <link href="css/bootstrap.min.css" rel="stylesheet">
+
+        <link href="css/index.css" rel="stylesheet">
+
+
+        <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+        <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    </head>
+    <body>
+    <nav class="navbar navbar-default">
+        <div class="container">
+            <div class="navbar-header">
+                <div>
+                    <a class="navbar-brand" href="#">
+                     <img alt="Wegro" src="img/wegrobanner.png" width="75" height="25">
+                    </a>
+                <div class="col-xs-1 pull-right">
+               <a href="login.php">  <button  type="button"  class="btn btn-default navbar-btn">Login</button> </a>
+            </div>
+        </div>
+    </nav>
+
