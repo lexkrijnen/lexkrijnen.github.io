630c48ca5273237850d9e37a1410fbedb1a08228
Mark Jansen <Markxjansen@gmail.com>
2017-11-30 15:10:09 +0100

    landing page

--- login.php
+++ login.php
@@ -49,13 +49,13 @@
     </div><!-- /.container-fluid -->
 </nav>
 <div class="container">
-    <div class="color">
+
+
     <div id="loginbox" style="margin-top:50px;" class="mainbox col-md-6 col-md-offset-3 col-sm-8 col-sm-offset-2">
         <div class="panel panel-info" >
             <div class="panel-heading">
                 <div class="panel-title">Log hier in met uw Wegro account</div>
             </div>
-        </div>
             <div style="padding-top:30px" class="panel-body" >
 
                 <div style="display:none" id="login-alert" class="alert alert-danger col-sm-12"></div>
