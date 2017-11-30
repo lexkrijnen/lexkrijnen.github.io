7e9ce79200de276e28025dead6cc7d9a34f063fa
Mark Jansen <Markxjansen@gmail.com>
2017-11-30 13:21:42 +0100

    landing page

--- index.php
+++ index.php
@@ -1,82 +1,89 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <meta name="description" content="Welkom bij Bouwbedrijf Wegro.">
+    <meta name="author" content="Nard Wemes">
+    <link rel="icon" href="images/logo-default.png">
 
-    <!DOCTYPE html>
-    <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
-        <meta name="description" content="Pagina beschrijving">
-        <meta name="author" content="Auteur">
-        <link rel="icon" href="images/favicon.ico">
+    <title>Bouwbedrijf Wegro</title>
 
-        <title>Welkom bij wegro</title>
+    <!-- Bootstrap core CSS -->
+		<link href="css/bootstrap.min.css" rel="stylesheet">
 
-        <link href="css/bootstrap.min.css" rel="stylesheet">
+    <!-- Global styles for this website -->
+    <link href="css/global.css" rel="stylesheet">
 
-        <link href="css/index.css" rel="stylesheet">
+    <!-- Custom styles for this page -->
+    <link href="css/index.css" rel="stylesheet">
 
-        <link href="css/global.css" rel="stylesheet">
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+	</head>
+  <body>
+  	<nav class="navbar navbar-default" role="navigation">
+			<div class="container">
+				<!-- Brand and toggle get grouped for better mobile display -->
+				<div class="navbar-header">
+					<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+						<span class="sr-only">Toggle navigation</span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+					</button>
+					<a class="navbar-brand" href="index.php"><img class="brand-logo" src="images/logo-nav.png" alt="logo"></a>
+				</div>
 
-
-        <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
-        <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-    </head>
-    <body>
-    <nav class="navbar navbar-default" role="navigation">
-        <div class="container">
-            <!-- Brand and toggle get grouped for better mobile display -->
-            <div class="navbar-header">
-                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-                    <span class="sr-only">Toggle navigation</span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                </button>
-                <a class="navbar-brand" href="index.php"><img class="brand-logo" src="img/wegrobanner.png" alt="logo"></a>
-            </div>
-
-            <!-- Collect the nav links, forms, and other content for toggling -->
-            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-                <ul class="nav navbar-nav navbar-right">
-                    <li class="nav-item"><a href="login.php">Inloggen</a></li>
-                </ul>
-            </div><!-- /.navbar-collapse -->
-        </div><!-- /.container-fluid -->
-    </nav>
-
-
+				<!-- Collect the nav links, forms, and other content for toggling -->
+				<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+					<ul class="nav navbar-nav navbar-right">
+						<li class="nav-item"><a href="#">Inloggen</a></li>
+					</ul>
+				</div><!-- /.navbar-collapse -->
+			</div><!-- /.container-fluid -->
+		</nav>
     <div class="container">
+    
+    	<div class="row">
+    		<div class="col-xs-10">
+    			<p class="page-question">Waarmee kan ik u van dienst zijn?</p>
+    		</div>
+    	</div>
+    	
+    	<div class="row">
+    		<div class="col-xs-4 col-xs-offset-1 col-md-3 col-md-offset-0 btn btn-default select-btn">
+    			<img class="select-logo" src="images/logo-white.png" alt="logo">
+    			<p class="select-title text-center">BOUWEN</p>
+    		</div>
+    		<div class="col-xs-4 col-xs-offset-1 col-md-3 btn btn-default select-btn">
+    			<img class="select-logo" src="images/logo-white.png" alt="logo">
+    			<p class="select-title text-center">TEKENEN</p>
+    		</div>
+    		<div class="col-xs-4 col-xs-offset-1 col-md-3 btn btn-default select-btn">
+    			<img class="select-logo" src="images/logo-white.png" alt="logo">
+    			<p class="select-title text-center">METSELEN</p>
+    		</div>
+    	</div>
+    
+    </div><!-- /.container -->
+      
+		<div class="row">
+			<div class="col-xs-12 text-center footer-rights">
+				<p>© Bouwbedrijf Wegro - Powered by <a href="#">Bootstrap</a> and <a href="#">Glyphicons</a>.</p>
+			</div>
+		</div>
+       
+        
+		<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+		<script src="js/jquery.min.js"></script>
 
-        <div class="row">
-            <div class="col-xs-10">
-                <p class="page-question">Welkom bij Wegro</p>
-            </div>
-        </div>
-
-        <div class="row">
-            <div class="col-xs-4 col-xs-offset-1 col-md-3 col-md-offset-0 btn btn-default select-btn">
-                <img class="select-logo" src="img/MBtegel2.png" alt="logo">
-
-            </div>
-            <div class="col-xs-4 col-xs-offset-1 col-md-3  btn btn-default select-btn">
-                <img class="select-logo" src="img/BBtegel2.png" alt="logo">
-
-            </div>
-            <div class="col-xs-4 col-xs-offset-1 col-md-3 btn btn-default select-btn">
-                <img class="select-logo" src="img/TBtegel2.png" alt="logo">
-            </div>
-
-        </div>
-
-    </div
-    <div class="row">
-        <div class="col-xs-12 text-center footer-rights">
-            <p>© Bouwbedrijf Wegro - Powered by <a href="#">Bootstrap</a> and <a href="#">Glyphicons</a>.</p>
-        </div>
-    </div>
-
-<script src="js/jquery.min.js"></script>
-<script src="js/bootstrap.min.js"></script>
-</body>
+		<!-- Bootstrap Framework -->
+		<script src="js/bootstrap.min.js"></script>
+	</body>
 </html>
--- login.php
+++ login.php
@@ -1,56 +1,89 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="Pagina beschrijving">
-    <meta name="author" content="Auteur">
-    <link rel="icon" href="images/favicon.ico">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <meta name="description" content="Welkom bij Bouwbedrijf Wegro.">
+    <meta name="author" content="Nard Wemes">
+    <link rel="icon" href="images/logo-default.png">
 
-    <title>Welkom bij wegro</title>
+    <title>Bouwbedrijf Wegro</title>
 
+    <!-- Bootstrap core CSS -->
     <link href="css/bootstrap.min.css" rel="stylesheet">
 
-    <link href="css/index.css" rel="stylesheet">
-
+    <!-- Global styles for this website -->
     <link href="css/global.css" rel="stylesheet">
 
+    <!-- Custom styles for this page -->
+    <link href="css/index.css" rel="stylesheet">
 
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
     <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
     <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-    <body class="center-block">
-<div class="centered">
-<div class="col-md-4">
-    <div class="login-panel panel panel-default">
-        <div class="panel-heading tegel">
-            <h3 class="panel-title">Inloggen bij uw Wegro account</h3>
+    <![endif]-->
+</head>
+<body>
+<nav class="navbar navbar-default" role="navigation">
+    <div class="container">
+        <!-- Brand and toggle get grouped for better mobile display -->
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="index.php"><img class="brand-logo" src="images/logo-nav.png" alt="logo"></a>
         </div>
-        <div class="panel-body">
-            <form role="form">
-                <fieldset>
-                    <div class="form-group">
-                        <input class="form-control" placeholder="E-mail" name="email" type="email" autofocus="">
-                    </div>
-                    <div class="form-group">
-                        <input class="form-control" placeholder="Wachtwoord" name="wachtwoord" type="password" value="">
-                    </div>
-                    <div class="checkbox">
-                        <label>
-                            <input name="remember" type="checkbox" value="Remember me">Onthoud mij
-                        </label>
-                    </div>
-                    <!-- Change this to a button or input when using this as a form -->
-                    <a href="javascript:;" class="btn btn-sm btn-success">Login</a>
-                </fieldset>
-            </form>
+
+        <!-- Collect the nav links, forms, and other content for toggling -->
+        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+            <ul class="nav navbar-nav navbar-right">
+                <li class="nav-item"><a href="#">Inloggen</a></li>
+            </ul>
+        </div><!-- /.navbar-collapse -->
+    </div><!-- /.container-fluid -->
+</nav>
+<div class="container">
+
+    <div class="row">
+        <div class="col-xs-10">
+            <p class="page-question">Waarmee kan ik u van dienst zijn?</p>
         </div>
     </div>
+
+    <div class="row">
+        <div class="col-xs-4 col-xs-offset-1 col-md-3 col-md-offset-0 btn btn-default select-btn">
+            <img class="select-logo" src="images/logo-white.png" alt="logo">
+            <p class="select-title text-center">BOUWEN</p>
+        </div>
+        <div class="col-xs-4 col-xs-offset-1 col-md-3 btn btn-default select-btn">
+            <img class="select-logo" src="images/logo-white.png" alt="logo">
+            <p class="select-title text-center">TEKENEN</p>
+        </div>
+        <div class="col-xs-4 col-xs-offset-1 col-md-3 btn btn-default select-btn">
+            <img class="select-logo" src="images/logo-white.png" alt="logo">
+            <p class="select-title text-center">METSELEN</p>
+        </div>
+    </div>
+
+</div><!-- /.container -->
+
+<div class="row">
+    <div class="col-xs-12 text-center footer-rights">
+        <p>© Bouwbedrijf Wegro - Powered by <a href="#">Bootstrap</a> and <a href="#">Glyphicons</a>.</p>
+    </div>
 </div>
-</div>
+
+
+<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+<script src="js/jquery.min.js"></script>
+
+<!-- Bootstrap Framework -->
+<script src="js/bootstrap.min.js"></script>
 </body>
-    <script src="js/jquery.min.js"></script>
-    <script src="js/bootstrap.min.js"></script>
-    </body>
 </html>
