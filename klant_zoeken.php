932afabcfe6596bc060bc18501562c1060453e21
daandejonge <daandejonge2000@live.nl>
2017-12-01 10:40:48 +0100

    verbinden database

--- klant_zoeken.php
+++ klant_zoeken.php
@@ -4,8 +4,8 @@ session_start();
 
 
 $db = "mysql:host=localhost; dbname=wegro; port=3306";
-$user = "root";
-$pass = "";
+$user = "wegro";
+$pass = "SQLWegro@101";
 $pdo = new PDO($db, $user, $pass);
 
 
