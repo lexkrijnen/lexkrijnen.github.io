06580b145a58522d55238625c42717aabd2d5ec0
daandejonge <daandejonge2000@live.nl>
2017-12-01 10:57:06 +0100

    probleem oplossen

--- klant_zoeken.php
+++ klant_zoeken.php
@@ -1,4 +1,37 @@
 
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <meta name="description" content="Welkom bij Bouwbedrijf Wegro.">
+    <meta name="author" content="Nard Wemes">
+    <link rel="icon" href="images/Logo%20bouwbedrijf%20Wegro.png">
+
+    <title>Welkom bij Wegro</title>
+
+    <!-- Bootstrap core CSS -->
+		<link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Global styles for this website -->
+    <link href="css/global.css" rel="stylesheet">
+
+    <!-- Custom styles for this page -->
+    <link href="css/klant_pagina.css" rel="stylesheet">
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+	</head>
+
+
 <?php
 session_start();
 
@@ -42,36 +75,6 @@ if (isset($_GET["vinden"])) {
 $pdo = NULL;
 ?>
 
-
-
-<!DOCTYPE html>
-<html lang="en">
-	<head>
-		<meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
-    <meta name="description" content="Welkom bij Bouwbedrijf Wegro.">
-    <meta name="author" content="Nard Wemes">
-    <link rel="icon" href="images/Logo%20bouwbedrijf%20Wegro.png">
-
-    <title>Welkom bij Wegro</title>
-
-    <!-- Bootstrap core CSS -->
-		<link href="css/bootstrap.min.css" rel="stylesheet">
-
-    <!-- Global styles for this website -->
-    <link href="css/global.css" rel="stylesheet">
-
-    <!-- Custom styles for this page -->
-    <link href="css/klant_pagina.css" rel="stylesheet">
-
-    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-    <!--[if lt IE 9]>
-      <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
-      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-    <![endif]-->
-	</head>
   <body>
   	<nav class="navbar navbar-default" role="navigation">
         <div class="container">
