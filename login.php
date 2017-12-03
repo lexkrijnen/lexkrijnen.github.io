0eece91372e807cdca9b61b3a85d1d554d0fce38
Mark Jansen <Markxjansen@gmail.com>
2017-12-03 13:46:09 +0100

    landing page

--- login.php
+++ login.php
@@ -1,19 +1,86 @@
 <?php
-$db = "mysql:localhost; dbname=wegro; port=3306";
+$db = "mysql:host=localhost; dbname=wegro; port=3306";
 $user = "wegro";
 $pass = "SQLWegro@101";
 $pdo = new PDO($db, $user, $pass);
 
+$stmt = $pdo->prepare("SELECT e-mailadress, wachtwoord FROM klant");
+$stmt->execute();
+$klanten = $stmt->fetchAll();
 
-$message="";
-if(!empty($_POST["submit"])) {
-    $result = mysqli_query($conn,"SELECT * FROM wegro WHERE e-mailadres='" . $_POST["e-mailadres"] . "' and wachtwoord = '". $_POST["wachtwoord"]."'");
-    $row  = mysqli_fetch_array($result);
-    if(is_array($row)) {
-        $_SESSION["user_id"] = $row['user_id'];
-    } else {
-        $message = "Uw email of wachtwoord is onjuist!";
+// Include config file
+require_once 'config.php';
+
+// Define variables and initialize with empty values
+$username = $password = "";
+$username_err = $password_err = "";
+
+// Processing form data when form is submitted
+if($_SERVER["REQUEST_METHOD"] == "POST"){
+
+    // Check if username is empty
+    if(empty(trim($_POST["e-mailadres"]))){
+        $username_err = 'Voer een geldige e-mailadres in.';
+    } else{
+        $username = trim($_POST["e-mailadres"]);
     }
+
+    // Check if password is empty
+    if(empty(trim($_POST['wachtwoord']))){
+        $password_err = 'Voer een geldig wachtwoord in.';
+    } else{
+        $password = trim($_POST['wachtwoord']);
+    }
+
+    // Validate credentials
+    if(empty($username_err) && empty($password_err)){
+        // Prepare a select statement
+        $sql = "SELECT e-mailadres, wachtwoord FROM klant WHERE e-mailadres = ?";
+
+        if($stmt = mysqli_prepare($link, $sql)){
+            // Bind variables to the prepared statement as parameters
+            mysqli_stmt_bind_param($stmt, "s", $param_username);
+
+            // Set parameters
+            $param_username = $username;
+
+            // Attempt to execute the prepared statement
+            if(mysqli_stmt_execute($stmt)){
+                // Store result
+                mysqli_stmt_store_result($stmt);
+
+                // Check if username exists, if yes then verify password
+                if(mysqli_stmt_num_rows($stmt) == 1){
+                    // Bind result variables
+                    mysqli_stmt_bind_result($stmt, $username, $hashed_password);
+                    if(mysqli_stmt_fetch($stmt)){
+                        if(password_verify($password, $hashed_password)){
+                            /* Password is correct, so start a new session and
+                            save the username to the session */
+                            session_start();
+                            $_SESSION['e-mailadres'] = $username;
+                            header("location: account.php");
+                        } else{
+                            // Display an error message if password is not valid
+                            $password_err = 'Het wachtwoord is incorrect.';
+                        }
+                    }
+                } else{
+                    // Display an error message if username doesn't exist
+                    $username_err = 'Er bestaan geen accounts met dit e-mailadres.';
+                }
+            } else{
+                echo "Oops! Something went wrong. Please try again later.";
+            }
+        }
+
+        // Close statement
+        mysqli_stmt_close($stmt);
+    }
+
+    // Close connection
+    mysqli_close($link);
+}
 }
 ?>
 <!DOCTYPE html>
