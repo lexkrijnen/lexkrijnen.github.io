ebb54c6295a6182e9d2693aadf6a64b5de49740b
Mark Jansen <Markxjansen@gmail.com>
2017-12-03 14:04:45 +0100

    landing page

--- dbconnect.php
+++ dbconnect.php
@@ -4,5 +4,8 @@ $db = "mysql:host=localhost; dbname=wegro; port=3306";
 $user = "wegro";
 $pass = "SQLWegro@101";
 $pdo = new PDO($db, $user, $pass);
+$sql = "SELECT * FROM `klant`";
+print("$sql");
 ?>
+
 
