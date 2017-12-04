82f525cce7830f8fba9ab495b1b176752f27bca4
Mark Jansen <Markxjansen@gmail.com>
2017-12-04 20:13:03 +0100

    landing page

--- BBhome.php
+++ BBhome.php
@@ -108,22 +108,17 @@
       </div>
           <div class="container">
           <div class="row">
+              <div>
 <div class="col-xs-10 col-md-5 col-md-offset-1">
     <div class="fb-page" data-href="https://www.facebook.com/Bouwbedrijf-Wegro-1708331486161176/?ref=br_rs"
          data-tabs="timeline" data-width="600" data-height="500" data-small-header="true" data-adapt-container-width="true"
          data-hide-cover="true" data-show-facepile="false"><blockquote cite="https://www.facebook.com/Bouwbedrijf-Wegro-1708331486161176/?ref=br_rs"
-                                                                       class="fb-xfbml-parse-ignore"><a href="https://www.facebook.com/Bouwbedrijf-Wegro-1708331486161176/?ref=br_rs">Bouwbedrijf Wegro</a></blockquote></div>
+                                                                      class="fb-xfbml-parse-ignore"><a href="https://www.facebook.com/Bouwbedrijf-Wegro-1708331486161176/?ref=br_rs">Bouwbedrijf Wegro</a></blockquote></div>
  </div>
-</div>
-          <div class="col-xs-10 col-md-5 col-md-offset-1">
-              <div class="fb-page" data-href="https://www.facebook.com/Bouwbedrijf-Wegro-1708331486161176/?ref=br_rs"
-                   data-tabs="timeline" data-width="600" data-height="500" data-small-header="true" data-adapt-container-width="true"
-                   data-hide-cover="true" data-show-facepile="false"><blockquote cite="https://www.facebook.com/Bouwbedrijf-Wegro-1708331486161176/?ref=br_rs"
-                                                                                 class="fb-xfbml-parse-ignore"><a href="https://www.facebook.com/Bouwbedrijf-Wegro-1708331486161176/?ref=br_rs">Bouwbedrijf Wegro</a></blockquote></div>
-          </div>
-      </div>
-  </div>
+              </div>
 
+          </div>
+          </div>
           <footer>
 
               <div class="footer-top">
