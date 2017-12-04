644d452bab0c5852e8fcc8b0ea122b3ec2887f66
marcprijs <33896960+marcprijs@users.noreply.github.com>
2017-12-04 17:55:20 +0100

    opnieuw

--- test_tabel_profile.php
+++ test_tabel_profile.php
@@ -90,11 +90,6 @@
     		</div>
     	</div>
 
-            	<div class="row">
-    		<div class="col-xs-10 col-xs-offset-1 col-md-3 col-md-offset-0 page-box">
-    			Hier komt een menu
-    		</div>
-
     		<div id="viewer-box" class="col-xs-10 col-xs-offset-1 col-md-8 page-box">
     			<iframe class="pdf-viewer" src="pdf-viewer/web/viewer.html?file=/pdf/test.pdf"></iframe>
 
@@ -104,7 +99,6 @@
 						<a class="btn btn-primary" onclick="window.open('pdf-viewer/web/viewer.html?file=/pdf/test.pdf', 'newwindow', 'width=600,height=1000'); return false;">Openen in nieuw scherm.</a>
     			</div>
     		</div>
-    	</div>
 
     </div><!-- /.container -->
 
