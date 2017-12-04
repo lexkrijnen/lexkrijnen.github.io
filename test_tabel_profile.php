9b41a2d1660a0468fb08e47e6322ee7e3ea6974d
marcprijs <33896960+marcprijs@users.noreply.github.com>
2017-12-04 16:25:35 +0100

    test

--- test_tabel_profile.php
+++ test_tabel_profile.php
@@ -1,5 +1,27 @@
 <!DOCTYPE html>
-<?php
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <meta name="description" content="Welkom bij Bouwbedrijf Wegro.">
+    <meta name="author" content="Nard Wemes">
+    <link rel="icon" href="images/Logo%20bouwbedrijf%20Wegro.png">
+
+
+    <title>Welkom bij Wegro</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Global styles for this website -->
+    <link href="css/global.css" rel="stylesheet">
+
+    <!-- Custom styles for this page -->
+    <link href="css/profile.css" rel="stylesheet">
+
+    <?php
         $db = "mysql:host=localhost; dbname=wegro; port=3306";
         $user = "root";
         $pass = "";
@@ -20,26 +42,6 @@
         $stmt->execute();
         $meerwerk = $stmt->fetchAll();
         ?>
-<html lang="en">
-	<head>
-		<meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
-    <meta name="description" content="Welkom bij Bouwbedrijf Wegro.">
-    <meta name="author" content="Nard Wemes">
-    <link rel="icon" href="images/Logo%20bouwbedrijf%20Wegro.png">
-
-    <title>Welkom bij Wegro</title>
-
-    <!-- Bootstrap core CSS -->
-    <link href="css/bootstrap.min.css" rel="stylesheet">
-
-    <!-- Global styles for this website -->
-    <link href="css/global.css" rel="stylesheet">
-
-    <!-- Custom styles for this page -->
-    <link href="css/profile.css" rel="stylesheet">
 
     <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
     <!--[if lt IE 9]>
