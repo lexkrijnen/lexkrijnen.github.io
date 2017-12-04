b0853a0f89a25a55e1e81ff2303b549f52a93fe6
Mark Jansen <Markxjansen@gmail.com>
2017-12-04 21:21:28 +0100

    landing page

--- BBhome.php
+++ BBhome.php
@@ -109,26 +109,7 @@
             </p>
     </div>
  </div>
-<div class="row">
-<div class="col-xs-10 col-md-6 col-md-offset-1">
-    <div class="fb-page" data-href="https://www.facebook.com/Bouwbedrijf-Wegro-1708331486161176/?ref=br_rs"
-      </div>
-          <div class="container">
-          <div class="row">
-              <div class="page-box col-xs-12">
 
-
-    <div class="fb-page col-xs-10 col-md-5 col-md-offset-1" data-href="https://www.facebook.com/Bouwbedrijf-Wegro-1708331486161176/?ref=br_rs"
-         data-tabs="timeline" data-width="600" data-height="500" data-small-header="true" data-adapt-container-width="true"
-         data-hide-cover="true" data-show-facepile="false">
-        <blockquote cite="https://www.facebook.com/Bouwbedrijf-Wegro-1708331486161176/?ref=br_rs"
-                    class="fb-xfbml-parse-ignore"><a href="https://www.facebook.com/Bouwbedrijf-Wegro-1708331486161176/?ref=br_rs">Bouwbedrijf Wegro</a></blockquote></div>
- </div>
-</div>
-</div>
-
-
-          </div>
 
   <footer id="footer-Section">
       <div class="footer-top-layout">
