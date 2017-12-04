50458f21f10c05d01b5eca921f7cd001f03ef5fe
daandejonge <daandejonge2000@live.nl>
2017-12-04 13:18:53 +0100

    db aangepast

--- klant_toevoegen.php
+++ klant_toevoegen.php
@@ -1,9 +1,9 @@
 
 <?php
 
-$db = "mysql:host=localhost; dbname=wegro; port=3306";
-$user = "root";
-$pass = "";
+$db = "mysql:host=localhost; dbname=Wegro; port=3306";
+$user = "wegro";
+$pass = "SQLWegro@101";
 $pdo = new PDO($db, $user, $pass);
 
 
