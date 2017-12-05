93115dcd80e32cac3d40ad68af40ceba3fd11eb8
Mark Jansen <Markxjansen@gmail.com>
2017-12-05 12:33:08 +0100

    landing page

--- BBhome.php
+++ BBhome.php
@@ -113,28 +113,20 @@
     </div>
  </div>
   <div class="row">
-  <div class="col-xs-10 col-xs-offset-1 col-md-5 col-md-offset-1  page-box">
+  <div class="col-xs-10 col-xs-offset-1  page-box">
       <div class="fb-page col-xs-10 col-md-5 col-md-offset-1" data-href="https://www.facebook.com/Bouwbedrijf-Wegro-1708331486161176/?ref=br_rs"
            data-tabs="timeline" data-width="500" data-height="500" data-small-header="true" data-adapt-container-width="true"
            data-hide-cover="true" data-show-facepile="false">
           <blockquote cite="https://www.facebook.com/Bouwbedrijf-Wegro-1708331486161176/?ref=br_rs"
                       class="fb-xfbml-parse-ignore"><a href="https://www.facebook.com/Bouwbedrijf-Wegro-1708331486161176/?ref=br_rs">Bouwbedrijf Wegro</a></blockquote>
       </div>
-
-
-  </div>
-  </div>
-  </div>
-  <div class="row">
-      <div class="col-xs-10 col-xs-offset-1 col-md-5 col-md-offset-1 page-box">
-          <div class="col-xs-10 col-md-5 col-md-offset-1">
-              <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2437.8785819838254!2d5.6273177160211905!3d52.33634875757526!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47c633c22a95401d%3A0xc7d3b89f5cf90a07!2sBouwbedrijf+Wegro+B.V.!5e0!3m2!1snl!2snl!4v1512472128799"
-                      width="500" height="500" frameborder="0" style="border:0" allowfullscreen></iframe>
-          </div>
-
+      <div class="col-xs-10 col-md-5 col-md-offset-1">
+          <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2437.8785819838254!2d5.6273177160211905!3d52.33634875757526!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47c633c22a95401d%3A0xc7d3b89f5cf90a07!2sBouwbedrijf+Wegro+B.V.!5e0!3m2!1snl!2snl!4v1512472128799"
+                  width="500" height="500" frameborder="0" style="border:0" allowfullscreen></iframe>
       </div>
-  </div>
 
+  </div>
+  </div>
 
 
   <footer id="footer-Section">
@@ -143,7 +135,7 @@
               <div class="row">
                   <div class="OurBlog">
                       <h4>Bouwbedrijf Wegro</h4>
-                      <p>Klik hieronder op het e-mailadres om contact op te nemen </p>
+                      <p>Klik hieronder op het e-mailadres om contact op te nemen</p>
 
                   </div>
                   <div class=" col-lg-8 col-lg-offset-2">
