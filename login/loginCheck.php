feb6ce4bcd048e191b09f20cbcb40750c63b876d
lexkrijnen <33865169+lexkrijnen@users.noreply.github.com>
2017-12-05 15:54:49 +0100

    login try and error lvl 19238018723

--- login/loginCheck.php
+++ login/loginCheck.php
@@ -27,7 +27,7 @@ if(isset($_POST['btn-login']))
         print("U bent niet ingelogt.");
     } else {
         print("Een moment geduld A.U.B.<br>");
-        print('<nav class="navbar navbar-default" role="navigation"><div class="container"><div class="navbar-header"><button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false"><span class="sr-only">Toggle navigation</span><span class="icon-bar"></span><span class="icon-bar"></span><span class="icon-bar"></span></button><a class="navbar-brand" href="index.php"><img class="brand-logo" src="images/wegrobanner.png" alt="logo"></a></div><div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1"></div></div></nav>');
+        print('<nav class="navbar navbar-default" role="navigation"><div class="container"><div class="navbar-header"><button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false"><span class="sr-only">Toggle navigation</span><span class="icon-bar"></span><span class="icon-bar"></span><span class="icon-bar"></span></button><a class="navbar-brand" href="../index.php"><img class="brand-logo" src="../images/wegrobanner.png" alt="logo"></a></div><div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1"></div></div></nav>');
         print('<meta http-equiv="refresh" content="50;url=../account.php" />');
         foreach ($queryresult as $a => $b){
             $klant_id = $b['klant_nummer'];
