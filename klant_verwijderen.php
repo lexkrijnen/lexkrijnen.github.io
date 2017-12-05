cf7b9f3fa9df378f4319dc4bd1315733d9d0cae1
daandejonge <daandejonge2000@live.nl>
2017-12-05 13:55:09 +0100

    terug knop in de container gezet

--- klant_verwijderen.php
+++ klant_verwijderen.php
@@ -91,12 +91,16 @@ $pdo = NULL;
 
         <div class=container>
 
-            <?php print("weet u zeker dat u " . $_SESSION["naam"] . " wilt verwijderen?"); ?>
+            <?php print("Weet u zeker dat u " . $_SESSION["naam"] . " wilt verwijderen?"); ?>
 
             <form action="klant_verwijderen.php" method="get">
                 <input class="btn btn-danger" type="submit" name="echtverwijderen" value="klant verwijderen">
                 <input class="btn btn-primary" type="button" value="annuleren" onclick="window.location.href='klant_zoeken.php'"/>
             </form>
+
+            <br>
+            <a href="klant_zoeken.php">terug</a>
+
         </div>
 
 
@@ -109,8 +113,7 @@ $pdo = NULL;
         }
         ?>
 
-        <br>
-        <a href="klant_zoeken.php">terug
+
 
 
 
