85e040fddf93129b635e23f32d3b60122de8700a
marcprijs <33896960+marcprijs@users.noreply.github.com>
2017-12-05 10:46:51 +0100

    TEST2

--- test_tabel_profile.php
+++ test_tabel_profile.php
@@ -25,27 +25,6 @@
         <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
         <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
         <![endif]-->
-        <?php
-        $db = "mysql:host=localhost; dbname=wegro; port=3306";
-        $user = "root";
-        $pass = "";
-        $pdo = new PDO($db, $user, $pass);
-
-        if (isset($_GET["opslaan"])) {
-            $sql = "UPDATE mutatie SET beschrijving=?, prijs=? WHERE mutatie_id=?";
-            $stmt = $pdo->prepare($sql);
-            $stmt->execute(array($_GET["beschrijving"], $_GET["prijs"], $_GET["nummer"]));
-            print('<div class="container"><div class="col-xs-4"><p>De wijzigingen zijn opgeslagen.</p></div></div>');
-            ## print('<meta http-equiv="refresh" content="2;url=http://localhost/WEGRO.Sandbox/index.php" />');
-        }
-
-        $sql = "SELECT * FROM mutatie WHERE mutatie_id=?";
-        $stmt = $pdo->prepare($sql);
-        $stmt->execute(array($_GET["nummer"]));
-        $werk = $stmt->fetch();
-
-        $pdo = NULL;
-        ?>
 	</head>
   <body>
   	<nav class="navbar navbar-default" role="navigation">
