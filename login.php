a8d429e321ff0819d79a555947831240725794fa
Mark Jansen <Markxjansen@gmail.com>
2017-12-05 11:01:51 +0100

    landing page

--- login.php
+++ login.php
@@ -13,15 +13,6 @@ if(isset($_GET['btn-login'])){
 
 print($username . $password);
 
-    if($username == "") {
-        $errMsg .= 'Vul een geldig e-mailadres in<br>';
-    }
-
-    if($password == "") {
-        $errMsg .= 'Vul een geldig wachtwoord in<br>';
-    }
-
-    if($errMsg == ""){
         $sql = ('SELECT e-mailadres, wachtwoord FROM  Klant WHERE e-mailadres = :e-mailadres');
         $stmt = $pdo->prepare($sql);
         $stmt->execute();
@@ -29,14 +20,12 @@ print($username . $password);
         $results = $stmt->fetch(PDO::FETCH_ASSOC);
         if(count($results) > 0 && password_verify($password, $results['wachtwoord'])){
             $_SESSION['e-mailadres'] = $results['e-mailadres'];
-            header('location:account.php');
+            header('location:/account.php');
             exit;
-        }else{
+        }else {
             $errMsg .= 'Username and Password are not found<br>';
         }
-    }
 }
-print("Error: " . $errormsg);
 ?>
 
 <!DOCTYPE html>
