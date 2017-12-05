7a54550c0a58876ea02fab70d0ca0cd7b7e62e72
lexkrijnen <33865169+lexkrijnen@users.noreply.github.com>
2017-12-05 15:52:40 +0100

    login try and error lvl 19238018723

--- login/loginCheck.php
+++ login/loginCheck.php
@@ -1,3 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="Welkom bij Bouwbedrijf Wegro.">
+    <meta name="author" content="Nard Wemes">
+    <link rel="icon" href="../images/Logo%20bouwbedrijf%20Wegro.png">
+    <title>Welkom bij Wegro</title>
+    <link href="../css/bootstrap.min.css" rel="stylesheet">
+    <link href="../css/global.css" rel="stylesheet">
+    <link href="../css/index.css" rel="stylesheet">
+    <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+</head>
+
 <?php
 session_start();
 include '../login/loginFunction.php';
