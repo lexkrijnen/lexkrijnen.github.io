fad3513a7fbdc4295b62c75e936b07039ff12e83
lexkrijnen <33865169+lexkrijnen@users.noreply.github.com>
2017-12-05 14:54:06 +0100

    login try and error lvl 19238018723

--- login.php
+++ login.php
@@ -1,31 +1,7 @@
 <!DOCTYPE html>
 <html>
-<?php
-session_start();
-include '../login/loginFunction.php';
-
-if(isset($_POST['submit']))
-{
-    $queryresult = GetLogin($_POST['emailadres'], $_POST['wachtwoord']);
-    var_dump($queryresult);
-
-    if (empty($queryresult)) {
-        print("U bent niet ingelogt.");
-    } else {
-        print("U bent ingelogt.<br>");
-        print('<meta http-equiv="refresh" content="2;url=../account.php" />');
-        foreach ($queryresult as $a => $b){
-            $klant_id = $b['klant_nummer'];
-            $klant_voornaam = $b['voornaam'];
-        }
-        $_SESSION['klant_id'] = $klant_id;
-        $_SESSION['voornaam'] = $klant_voornaam;
-    }
-}
-?>
-
 <body>
-<form action="login.php" method="POST">
+<form action="../login/loginCheck.php" method="POST">
     E-mailadres:<br>
     <input type="text" name="emailadres" value="">
     <br>
