6103dfcc6d6c46b40d36ba730dbf395492f19080
Mark Jansen <Markxjansen@gmail.com>
2017-12-06 15:30:48 +0100

    landing page

--- BBhome.php
+++ BBhome.php
@@ -210,7 +210,7 @@
 
           <div class="col-xs-10 col-md-5 map_canvas">
               <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2437.8785819838254!2d5.6273177160211905!3d52.33634875757526!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47c633c22a95401d%3A0xc7d3b89f5cf90a07!2sBouwbedrijf+Wegro+B.V.!5e0!3m2!1snl!2snl!4v1512472128799"
-                       frameborder="0" style="border:0" allowfullscreen></iframe>
+                      width="1300" height="400" frameborder="0" style="border:0" allowfullscreen></iframe>
           </div>
 
       </div>
