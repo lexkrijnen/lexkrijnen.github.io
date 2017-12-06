c19fdcc4b4f4bdd00599c25436d1a5dcf976bab7
Mark Jansen <Markxjansen@gmail.com>
2017-12-06 14:44:07 +0100

    landing page

--- BBhome.php
+++ BBhome.php
@@ -278,7 +278,7 @@
                 if( target.length ) {
                     event.preventDefault();
                     $('html, body').stop().animate({
-                        scrollTop: target.offset().top
+                        scrollTop: target.offset(75px).top
                     }, 1000);
                 }
             });
