9e5c9e217f841911568eb3bb4bbcb2e3ab3e6522
marcprijs <33896960+marcprijs@users.noreply.github.com>
2017-12-06 14:32:03 +0100

    tesy

--- test_tabel_profile.php
+++ test_tabel_profile.php
@@ -79,7 +79,9 @@
                 <?php
                 foreach ($meerwerk AS $werk) {
                     print("<tr>");
+                    print("<tbody>");
                     print("<td>" . $werk["beschrijving"] . "</td>");
+                    print("</tbody>")
                     print("</tr>");
                     }
                 ?>
@@ -89,12 +91,16 @@
     <form method="get" action="meermindertoevoegen.php">
         <table class="table table-hover table-bordered">
                 <tr>
-                    <th><h3><b>Tekeningen</b></h3></th>
+                    <thead>
+                        <th><h3><b>Tekeningen</b></h3></th>
+                    </thead>
                 </tr>
                 <?php
                 foreach ($minderwerk AS $werk2) {
                     print("<tr>");
+                    print("<tbody>");
                     print("<td>" . $werk2["beschrijving"] . "</td>");
+                    print("</tbody>")
                     print("</tr>");
                     }
                 ?>
