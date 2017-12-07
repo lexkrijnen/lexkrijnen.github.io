264f751cb0001322b52da7aa5d5b24e1be828b4a
Mark Jansen <Markxjansen@gmail.com>
2017-12-07 13:13:56 +0100

    landing page

--- BBhome.php
+++ BBhome.php
@@ -60,8 +60,8 @@
       <div class="row BBhomeBackground">
            <img src="images/Logo%20bouwbedrijf%20Wegro.png" class="logo">
          <a href="contact.php"> <a name="Ons_bedrijf" id="Ons_bedrijf"></a>
-                <div class="col-xs-2 col-xs-offset-5 btn contbutton">
-                    <button class="btn oranje white" href="contact.php">Contact</button>
+                <div class="col-xs-2 col-xs-offset-5 btn contbutton oranje white">
+                    Contact
                 </div>
             </a>
           </div>
