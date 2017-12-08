2c8775dd9d352cd4767acd6f5a93327e07913b62
daandejonge <daandejonge2000@live.nl>
2017-12-08 11:13:38 +0100

    class form control weggehaald bij radio

--- mw_toevoegen.php
+++ mw_toevoegen.php
@@ -164,7 +164,7 @@ $hash = sha1($salt . $wachtwoord);
           <tr>
             <td>Functie:</td>
             <td>
-                Medewerker<input type="radio" class="form-control" name="medewerker">
+                Medewerker<input type="radio" name="medewerker">
             </td>
           </tr>
           <tr>
@@ -172,7 +172,7 @@ $hash = sha1($salt . $wachtwoord);
 
             </td>
             <td>
-                Admin<input type="radio" class="form-control" name="medewerker">
+                Admin<input type="radio" name="medewerker">
             </td>
           </tr>
           <tr>
