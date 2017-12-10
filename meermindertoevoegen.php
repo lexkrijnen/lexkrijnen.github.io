359dbc780edce5e9b14dff2c657142b3eadd75f2
lexkrijnen <33865169+lexkrijnen@users.noreply.github.com>
2017-12-10 18:00:58 +0100

    Aanpassingen Meer/Minder werk

--- meermindertoevoegen.php
+++ meermindertoevoegen.php
@@ -163,7 +163,7 @@
                     <td></td>
                     <td><input type="text" name="beschrijving" size="15"></td>
                     <td><input type="text" name="prijs"size="3"></td>
-                    <td><input type="text" name="id" value="<?php print($_GET['id']);?>" disabled></td>
+                    <td><input type="hidden" name="id" value="<?php print($_GET['id']);?>" disabled></td>
                     <td><input type="submit" name="toevoegenminderwerk" value="Toevoegen"></td>
                     <td></td>
                 </tr>
