66df2d566a804aadd18c6ce973e25205e678b779
lexkrijnen <33865169+lexkrijnen@users.noreply.github.com>
2017-12-10 14:37:20 +0100

    gebeunt

--- meerminderinzien.php
+++ meerminderinzien.php
@@ -73,7 +73,9 @@
 <div class="container page-box">
     <div class="col-xs-4">
         <h1>Meer Werk</h1>
-        <?php print("<h5>Projectnaam: " . $naamproject . "</h5>"); ?>
+        <?php print("<h5>Projectnaam: " . $naamproject . "</h5>")
+        var_dump($naamproject);
+        ; ?>
         <form method="get" action="meermindertoevoegen.php">
             <table class="table table-hover table-bordered">
                 <tr>
