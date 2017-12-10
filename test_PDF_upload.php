892ccef641a5ec847971e11f15cebda54dc7dbd8
marcprijs <33896960+marcprijs@users.noreply.github.com>
2017-12-10 14:29:41 +0100

    toch goed

--- test_PDF_upload.php
+++ test_PDF_upload.php
@@ -84,6 +84,24 @@
                     }
                 ?>
             </table>
+                 <a href="#" class="btn btn-lg btn-success" data-toggle="modal" data-target="#basicModal">Bestand uploaden</a>
+                    <div class="modal fade" id="basicModal" tabindex="-1" role="dialog" aria-labelledby="basicModal" aria-hidden="true">
+                    <div class="modal-dialog">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+                            <h4 class="modal-title" id="myModalLabel">Contract uploaden</h4>
+                            </div>
+                            <div class="modal-body">
+                                <h3>Modal Body</h3>
+                            </div>
+                            <div class="modal-footer">
+                                <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                                <button type="button" class="btn btn-primary">Save changes</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
         </form>
     <br>
         <!--Tekening-->
