42794325edd08b7506c63547f5f3132bd40cb7d3
daandejonge <daandejonge2000@live.nl>
2017-12-11 19:05:46 +0100

    ;

--- bekijken_bouwtekeningen.php
+++ bekijken_bouwtekeningen.php
@@ -120,7 +120,7 @@ $pdo = NULL;
                 print("</ul>");
 
                 print("<div class='tab-content'>");
-                print("<div class='tab-pane' id='informatie'>")
+                print("<div class='tab-pane' id='informatie'>");
                 print("<table>");
                 print("<tr><td>Projectnaam:</td><td>$naam</td></tr>");
                 print("<tr><td>Projectnummer:</td><td>$project_nummer</td></tr>");
@@ -131,7 +131,7 @@ $pdo = NULL;
                 print("</table>");
                 print("</div>");
 
-                print("<div class='tab-pane' id='bouwtekeningen'>")
+                print("<div class='tab-pane' id='bouwtekeningen'>");
                 print("<img src=\"images/IMG_7017.JPG\" class=\"img-responsive\">");
                 print("<img src=\"images/IMG_7020.JPG\" class=\"img-responsive\">");
                 print("<img src=\"images/IMG_7028.JPG\" class=\"img-responsive\">");
