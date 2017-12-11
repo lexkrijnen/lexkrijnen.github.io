bce6043bf7271fea1d8e77377bfb51d04480b9be
marcprijs <33896960+marcprijs@users.noreply.github.com>
2017-12-11 15:25:04 +0100

    jaweh 6.0

--- test_tabel_profile.php
+++ test_tabel_profile.php
@@ -81,7 +81,7 @@
                 <?php
                 foreach ($contract AS $document) {
                     print("<tr>");
-                    print("<td>" . $document["document"] . "</td>");
+                    print("<td>" . $document["naam"] . "</td>");
                     print("</tr>");
                     }
                 ?>
@@ -99,7 +99,7 @@
                 <?php
                 foreach ($tekening AS $document2) {
                     print("<tr>");
-                    print("<td>" . $document2["document"] . "</td>");
+                    print("<td>" . $document2["naam"] . "</td>");
                     print("</tr>");
                     }
                 ?>
