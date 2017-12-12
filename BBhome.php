5702c872a69a7c6e5cce91a68e444ccf993cc8f7
Mark Jansen <Markxjansen@gmail.com>
2017-12-12 13:24:03 +0100

    landing page

--- BBhome.php
+++ BBhome.php
@@ -61,9 +61,9 @@
 		</nav>
     <!--<div class="">-->
     <div class="row">
-        <img src="images/Logo%20bouwbedrijf%20Wegro.png" class="logo zindex2">
+        <img src="images/Logo%20bouwbedrijf%20Wegro.png" class="logo">
         <a href="contact.php">
-            <div href="contact.php" class="col-xs-2 col-xs-offset-5 btn contbutton oranje zindex2">
+            <div href="contact.php" class="col-xs-2 col-xs-offset-5 btn contbutton oranje">
                 Contact
             </div>
         </a>
@@ -254,39 +254,7 @@
 
 
  </div>
-          <div id="fb-root"></div>
-          <script>(function(d, s, id) {
-                  var js, fjs = d.getElementsByTagName(s)[0];
-                  if (d.getElementById(id)) {return;}
-                  js = d.createElement(s); js.id = id;
-                  js.src = "//connect.facebook.net/en_US/all.js#xfbml=1";
-                  fjs.parentNode.insertBefore(js, fjs);
-              }(document, 'script', 'facebook-jssdk'));</script>
 
-
-          <div class="container">
-<div class="page-box">
-              <div class="row">
-                  <div id="fb-root">
-                      <div class=" fb-wrap col-xs-12 col-sm-12 col-md-6 col-lg-12">
-                          <div class="fb-page" data-href="https://www.facebook.com/Bouwbedrijf-Wegro-1708331486161176/?ref=br_rs"
-                               data-tabs="timeline" data-width="500" data-height="500" data-small-header="true" data-adapt-container-width="true"
-                               data-hide-cover="true" data-show-facepile="false">
-                              <blockquote cite="https://www.facebook.com/Bouwbedrijf-Wegro-1708331486161176/?ref=br_rs"
-                                          class="fb-xfbml-parse-ignore"><a href="https://www.facebook.com/Bouwbedrijf-Wegro-1708331486161176/?ref=br_rs">Bouwbedrijf Wegro</a></blockquote>
-                          </div>
-
-                          </div>
-                      </div>
-                  </div>
-    <div class="">
-        <div class="footer-col-item col-xs-12 col-sm-12 col-md-6 col-lg-12">
-            <iframe src="https://maps.googleapis.com/maps/api/staticmap?key=AIzaSyBwptpWh_rncSVZXBNSC8kylkcIDhdtprI&center=52.336564981820615,5.63063184795455&zoom=19&format=png&maptype=roadmap&style=element:geometry%7Ccolor:0x3c3c3b&style=element:labels.icon%7Cvisibility:off&style=element:labels.text%7Ccolor:0xfe9c15&style=element:labels.text.fill%7Ccolor:0xfe9c15%7Cvisibility:off&style=element:labels.text.stroke%7Ccolor:0xfe9c15&style=feature:administrative%7Celement:geometry%7Ccolor:0x757575&style=feature:administrative.country%7Celement:labels.text.fill%7Ccolor:0x9e9e9e&style=feature:administrative.land_parcel%7Cvisibility:off&style=feature:administrative.locality%7Celement:labels.text.fill%7Ccolor:0xbdbdbd&style=feature:poi%7Celement:labels.text.fill%7Ccolor:0x757575&style=feature:poi.park%7Celement:geometry%7Ccolor:0x181818&style=feature:poi.park%7Celement:labels.text.fill%7Ccolor:0x616161&style=feature:poi.park%7Celement:labels.text.stroke%7Ccolor:0x1b1b1b&style=feature:road%7Celement:geometry.fill%7Ccolor:0x2c2c2c&style=feature:road%7Celement:labels.text.fill%7Ccolor:0x8a8a8a&style=feature:road.arterial%7Celement:geometry%7Ccolor:0x373737&style=feature:road.highway%7Celement:geometry%7Ccolor:0x3c3c3c&style=feature:road.highway.controlled_access%7Celement:geometry%7Ccolor:0x4e4e4e&style=feature:road.local%7Celement:labels.text.fill%7Ccolor:0x616161&style=feature:transit%7Celement:labels.text.fill%7Ccolor:0x757575&style=feature:water%7Celement:geometry%7Ccolor:0x000000&style=feature:water%7Celement:labels.text.fill%7Ccolor:0x3d3d3d&size=480x360"
-                                  width="500" height="500" frameborder="0" style="border:0" allowfullscreen></iframe>
-        </div>
-    </div>
- </div>
-          </divpage-box>
   <footer id="footer-Section">
       <div class="footer-top-layout">
           <div class="container">
@@ -301,7 +269,12 @@
                               <div class="item-contact">  <a href="tel:0341-412054"><span class="link-id">TEL:</span>:<span>0341-412054</span></a> <br><a href="mailto:info@bouwbedrijfwegro.nl"><span class="link-id">MAIL</span>:<span>info@bouwbedrijfwegro.nl</span></a> </div>
                           </div>
                       </div>
-
+                      <div class="col-sm-3">
+                      <div class="footer-col-item">
+                          <iframe src="https://maps.googleapis.com/maps/api/staticmap?key=AIzaSyBwptpWh_rncSVZXBNSC8kylkcIDhdtprI&center=52.336564981820615,5.63063184795455&zoom=19&format=png&maptype=roadmap&style=element:geometry%7Ccolor:0x3c3c3b&style=element:labels.icon%7Cvisibility:off&style=element:labels.text%7Ccolor:0xfe9c15&style=element:labels.text.fill%7Ccolor:0xfe9c15%7Cvisibility:off&style=element:labels.text.stroke%7Ccolor:0xfe9c15&style=feature:administrative%7Celement:geometry%7Ccolor:0x757575&style=feature:administrative.country%7Celement:labels.text.fill%7Ccolor:0x9e9e9e&style=feature:administrative.land_parcel%7Cvisibility:off&style=feature:administrative.locality%7Celement:labels.text.fill%7Ccolor:0xbdbdbd&style=feature:poi%7Celement:labels.text.fill%7Ccolor:0x757575&style=feature:poi.park%7Celement:geometry%7Ccolor:0x181818&style=feature:poi.park%7Celement:labels.text.fill%7Ccolor:0x616161&style=feature:poi.park%7Celement:labels.text.stroke%7Ccolor:0x1b1b1b&style=feature:road%7Celement:geometry.fill%7Ccolor:0x2c2c2c&style=feature:road%7Celement:labels.text.fill%7Ccolor:0x8a8a8a&style=feature:road.arterial%7Celement:geometry%7Ccolor:0x373737&style=feature:road.highway%7Celement:geometry%7Ccolor:0x3c3c3c&style=feature:road.highway.controlled_access%7Celement:geometry%7Ccolor:0x4e4e4e&style=feature:road.local%7Celement:labels.text.fill%7Ccolor:0x616161&style=feature:transit%7Celement:labels.text.fill%7Ccolor:0x757575&style=feature:water%7Celement:geometry%7Ccolor:0x000000&style=feature:water%7Celement:labels.text.fill%7Ccolor:0x3d3d3d&size=480x360"
+                                  width="200" height="100" frameborder="0" style="border:0" allowfullscreen></iframe>
+                      </div>
+                  </div>
                       <div class="col-sm-3">
                           <div class="footer-col-item">
                               <h4>Locatie</h4>
