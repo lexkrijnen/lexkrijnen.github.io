29dee80ed099655d322920826247f07ca444d95d
daandejonge <daandejonge2000@live.nl>
2017-12-12 18:55:11 +0100

    value veranderd

--- bekijken_bouwtekeningen.php
+++ bekijken_bouwtekeningen.php
@@ -91,7 +91,7 @@ $pdo = NULL;
                 <tr>
                   <div class=row>
                     <form action="bekijken_bouwtekeningen.php" method="get">
-                        <td><input type="text" class="form-control" name="projectnaam" placeholder="projectnaam" <?php if(isset($_GET["vinden"])) {print("value=.$naam");}?>></td>
+                        <td><input type="text" class="form-control" name="projectnaam" placeholder="projectnaam" <?php if(isset($_GET["vinden"])) {print("value.=$naam");}?>></td>
                         <td><input class="btn oranje white" type="submit" name="vinden" value="vinden"></td>
                         <td><input type="hidden" name="project_nummer" <?php if (isset($_GET["vinden"])) { print("value=$project_nummer"); } ?>></td>
                     </form>
