23d26b1fef50b5814813dd0b04199607cb909695
daandejonge <daandejonge2000@live.nl>
2017-12-12 13:10:46 +0100

    projectnaam veranderd

--- bekijken_bouwtekeningen.php
+++ bekijken_bouwtekeningen.php
@@ -12,7 +12,7 @@ if (isset($_GET["vinden"])) {
 
     $project = $stmt->fetch();
 
-    $naam = $project["naam"];
+    $naam = $project["p.naam"];3
     $project_nummer = $project["project_nummer"];
     $status = $project["status_titel"];
     $klant_nummer = $project["klant_nummer"];
@@ -36,10 +36,10 @@ $pdo = NULL;
     <meta name="author" content="Nard Wemes">
     <link rel="icon" href="images/Logo%20bouwbedrijf%20Wegro.png">
 
-    <title>Welkom bij Wegro</title>
+    <title>bekijken bouwtekeningen</title>
 
     <!-- Bootstrap core CSS -->
-		<link href="css/bootstrap.min.css" rel="stylesheet">
+        <link href="css/bootstrap.min.css" rel="stylesheet">
 
     <!-- Global styles for this website -->
     <link href="css/global.css" rel="stylesheet">
