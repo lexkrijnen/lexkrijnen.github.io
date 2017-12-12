390457defa80597ed786d672e41573897c0392c1
Mark Jansen <Markxjansen@gmail.com>
2017-12-12 13:39:55 +0100

    landing page

--- BBhome.php
+++ BBhome.php
@@ -254,7 +254,14 @@
 
 
  </div>
-
+          <div class="page-box">
+          <div class="fb-page" data-href="https://www.facebook.com/Bouwbedrijf-Wegro-1708331486161176/?ref=br_rs"
+               data-tabs="timeline" data-width="500" data-height="500" data-small-header="false" data-adapt-container-width="true"
+               data-hide-cover="true" data-show-facepile="false">
+              <blockquote cite="https://www.facebook.com/Bouwbedrijf-Wegro-1708331486161176/?ref=br_rs" class="fb-xfbml-parse-ignore">
+                  <a href="https://www.facebook.com/Bouwbedrijf-Wegro-1708331486161176/?ref=br_rs">Bouwbedrijf Wegro</a></blockquote>
+          </div>
+          </div>
   <footer id="footer-Section">
       <div class="footer-top-layout">
           <div class="container">
@@ -331,22 +338,20 @@
             });
         </script>
   <script>
-      (function(d, s, id) {
-          var js, fjs = d.getElementsByTagName(s)[0];
-          if (d.getElementById(id)) return;
-          js = d.createElement(s); js.id = id;
-          js.src = 'https://connect.facebook.net/nl_NL/sdk.js#xfbml=1&version=v2.11';
-          fjs.parentNode.insertBefore(js, fjs);
-      }(document, 'script', 'facebook-jssdk'));
-  </script>
-
-  <script>
 		$(document).ready(function() {
 			$('.slider-demo').bscSlider({
 				effect: 1
 			});
 		})
 	</script>
+          <div id="fb-root"></div>
+          <script>(function(d, s, id) {
+                  var js, fjs = d.getElementsByTagName(s)[0];
+                  if (d.getElementById(id)) return;
+                  js = d.createElement(s); js.id = id;
+                  js.src = 'https://connect.facebook.net/nl_NL/sdk.js#xfbml=1&version=v2.11';
+                  fjs.parentNode.insertBefore(js, fjs);
+              }(document, 'script', 'facebook-jssdk'));</script>
   <!--
   <script>
       $(document).ready(function(){
