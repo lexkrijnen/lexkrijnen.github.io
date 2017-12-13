0b10858e2b311847170a3c2385ffa2d037f930bf
Mark Jansen <Markxjansen@gmail.com>
2017-12-13 14:26:03 +0100

    landing page

--- BBhome.php
+++ BBhome.php
@@ -215,8 +215,8 @@
 
  </div>
 
-    <div class="google-maps">
-    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2437.878581983825!2d5.627317716021184!3d52.336348757575266!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47c633c22a95401d%3A0xc7d3b89f5cf90a07!2sBouwbedrijf+Wegro+B.V.!5e0!3m2!1snl!2snl!4v1513166724420" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
+    <div class="google-maps" style=" width: 100%;">
+    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2437.878581983825!2d5.627317716021184!3d52.336348757575266!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47c633c22a95401d%3A0xc7d3b89f5cf90a07!2sBouwbedrijf+Wegro+B.V.!5e0!3m2!1snl!2snl!4v1513166724420" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
     </div>
     <footer id="footer-Section">
       <div class="footer-top-layout">
