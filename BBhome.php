ab80fa104b6e7c0c635ebdf5f8bf5d63b1fad0a4
Mark Jansen <Markxjansen@gmail.com>
2017-12-13 13:07:37 +0100

    landing page

--- BBhome.php
+++ BBhome.php
@@ -225,10 +225,9 @@
 
           </div>
     <div class="google-maps">
-        <iframe src="https://maps.googleapis.com/maps/api/staticmap?key=AIzaSyBwptpWh_rncSVZXBNSC8kylkcIDhdtprI&center=52.336564981820615,5.63063184795455&zoom=19&format=png&maptype=roadmap&style=element:geometry%7Ccolor:0x3c3c3b&style=element:labels.icon%7Cvisibility:off&style=element:labels.text%7Ccolor:0xfe9c15&style=element:labels.text.fill%7Ccolor:0xfe9c15%7Cvisibility:off&style=element:labels.text.stroke%7Ccolor:0xfe9c15&style=feature:administrative%7Celement:geometry%7Ccolor:0x757575&style=feature:administrative.country%7Celement:labels.text.fill%7Ccolor:0x9e9e9e&style=feature:administrative.land_parcel%7Cvisibility:off&style=feature:administrative.locality%7Celement:labels.text.fill%7Ccolor:0xbdbdbd&style=feature:poi%7Celement:labels.text.fill%7Ccolor:0x757575&style=feature:poi.park%7Celement:geometry%7Ccolor:0x181818&style=feature:poi.park%7Celement:labels.text.fill%7Ccolor:0x616161&style=feature:poi.park%7Celement:labels.text.stroke%7Ccolor:0x1b1b1b&style=feature:road%7Celement:geometry.fill%7Ccolor:0x2c2c2c&style=feature:road%7Celement:labels.text.fill%7Ccolor:0x8a8a8a&style=feature:road.arterial%7Celement:geometry%7Ccolor:0x373737&style=feature:road.highway%7Celement:geometry%7Ccolor:0x3c3c3c&style=feature:road.highway.controlled_access%7Celement:geometry%7Ccolor:0x4e4e4e&style=feature:road.local%7Celement:labels.text.fill%7Ccolor:0x616161&style=feature:transit%7Celement:labels.text.fill%7Ccolor:0x757575&style=feature:water%7Celement:geometry%7Ccolor:0x000000&style=feature:water%7Celement:labels.text.fill%7Ccolor:0x3d3d3d&size=480x360"
-                width="" height="" frameborder="0" style="border:0" allowfullscreen></iframe>
+    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2437.878581983825!2d5.627317716021184!3d52.336348757575266!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47c633c22a95401d%3A0xc7d3b89f5cf90a07!2sBouwbedrijf+Wegro+B.V.!5e0!3m2!1snl!2snl!4v1513166724420" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
     </div>
-  <footer id="footer-Section">
+    <footer id="footer-Section">
       <div class="footer-top-layout">
           <div class="container">
               <div class="row">
