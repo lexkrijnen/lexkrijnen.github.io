a9f0b2ea6108acee40c0b36bcbe0e38070cce74b
Mark Jansen <Markxjansen@gmail.com>
2017-12-13 12:49:10 +0100

    landing page

--- BBhome.php
+++ BBhome.php
@@ -238,11 +238,8 @@
                               <div class="item-contact">  <a href="tel:0341-412054"><span class="link-id">TEL:</span>:<span>0341-412054</span></a> <br><a href="mailto:info@bouwbedrijfwegro.nl"><span class="link-id">MAIL</span>:<span>info@bouwbedrijfwegro.nl</span></a> </div>
                           </div>
                       </div>
-                      <div class="col-sm-3">
-                      <div class="footer-col-item">
-                          <iframe src="https://maps.googleapis.com/maps/api/staticmap?key=AIzaSyBwptpWh_rncSVZXBNSC8kylkcIDhdtprI&center=52.336564981820615,5.63063184795455&zoom=19&format=png&maptype=roadmap&style=element:geometry%7Ccolor:0x3c3c3b&style=element:labels.icon%7Cvisibility:off&style=element:labels.text%7Ccolor:0xfe9c15&style=element:labels.text.fill%7Ccolor:0xfe9c15%7Cvisibility:off&style=element:labels.text.stroke%7Ccolor:0xfe9c15&style=feature:administrative%7Celement:geometry%7Ccolor:0x757575&style=feature:administrative.country%7Celement:labels.text.fill%7Ccolor:0x9e9e9e&style=feature:administrative.land_parcel%7Cvisibility:off&style=feature:administrative.locality%7Celement:labels.text.fill%7Ccolor:0xbdbdbd&style=feature:poi%7Celement:labels.text.fill%7Ccolor:0x757575&style=feature:poi.park%7Celement:geometry%7Ccolor:0x181818&style=feature:poi.park%7Celement:labels.text.fill%7Ccolor:0x616161&style=feature:poi.park%7Celement:labels.text.stroke%7Ccolor:0x1b1b1b&style=feature:road%7Celement:geometry.fill%7Ccolor:0x2c2c2c&style=feature:road%7Celement:labels.text.fill%7Ccolor:0x8a8a8a&style=feature:road.arterial%7Celement:geometry%7Ccolor:0x373737&style=feature:road.highway%7Celement:geometry%7Ccolor:0x3c3c3c&style=feature:road.highway.controlled_access%7Celement:geometry%7Ccolor:0x4e4e4e&style=feature:road.local%7Celement:labels.text.fill%7Ccolor:0x616161&style=feature:transit%7Celement:labels.text.fill%7Ccolor:0x757575&style=feature:water%7Celement:geometry%7Ccolor:0x000000&style=feature:water%7Celement:labels.text.fill%7Ccolor:0x3d3d3d&size=480x360"
-                                  width="200" height="100" frameborder="0" style="border:0" allowfullscreen></iframe>
-                      </div>
+                      <div class="col-sm-4">
+
                   </div>
                       <div class="col-sm-3">
                           <div class="footer-col-item">
