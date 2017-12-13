d6c3c7985b24f7be222ca58c10116899ffef4266
lexkrijnen <33865169+lexkrijnen@users.noreply.github.com>
2017-12-13 13:04:45 +0100

    FIX toevoegen meer/minder werk

---
+++ login/loginFunctionAdmin.php
@@ -0,0 +1,14 @@
+<?php
+session_start();
+function GetLogin($username, $password) {
+    $db = "mysql:host=localhost; dbname=Wegro; port=3306";
+    $user = "wegro";
+    $pass = "SQLWegro@101";
+    $pdo = new PDO($db, $user, $pass);
+
+    $stmt2 = $pdo->prepare("SELECT * FROM Medewerker WHERE emailadres = '$username' AND wachtwoord = '$password'");
+    $stmt2->execute();
+    $result = $stmt2->fetchAll();
+
+    return $result;
+}
