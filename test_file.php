8a9e4e5b98dad25f4fa43c547316786624c6f8a5
marcprijs <33896960+marcprijs@users.noreply.github.com>
2017-12-14 13:46:00 +0100

    testtesttest

--- test_file.php
+++ test_file.php
@@ -96,6 +96,7 @@
                     print("<td>" . $document["contract_nummer"] . "</td>");
                     print("<td>" . $document["document"] . "</td>");
                     print("<td>" . $document["naam"] . "</td>");
+                    print("<td>" . $)
                     print("</tr>");
                 }
                 ?>
@@ -108,46 +109,6 @@
             </table>
         </form>
         <a href="meerminderadminlanding.php"><button type="button" class="btn btn-primary btn-return">Terug naar overzicht</button></a>
-    </div>
-
-
-    <div class="col-xs-3"></div> <!-- LEGE RUIMTE TUSSEN KOLOMMEN-->
-
-
-    <!--MINDER WERK-->
-    <div class="col-xs-4">
-        <h1>Minder Werk</h1>
-        <form method="get" action="meermindertoevoegen.php">
-            <table class="table table-hover table-bordered">
-                <tr>
-                    <th>Nr.</th>
-                    <th>Beschrijving</th>
-                    <th>Prijs</th>
-                    <th></th>
-                    <th></th>
-                </tr>
-                <?php
-                $minderwerkcount = 1;
-                foreach ($minderwerk AS $werk2) {
-                    print("<tr>");
-                    print("<td>" . $minderwerkcount . "</td>");
-                    print("<td>" . $werk2["beschrijving"] . "</td>");
-                    print("<td>- € " . $werk2["prijs"] . "</td>");
-                    print("<td> <a href=\"meerminderbewerk.php?nummer=" . $werk2["mutatie_id"] . "&id=" . $werk2["contract_nummer"] . "\">Bewerk</a> </td>");
-                    print("<td> <a href=\"meerminderverwijder.php?nummer=" . $werk2["mutatie_id"] . "&id=" . $werk2["contract_nummer"] . "\">Verwijder</a></td>");
-                    print("</tr>");
-                    $minderwerkcount++;
-                }
-                ?>
-                <tr>
-                    <td></td>
-                    <td><input type="text" name="beschrijving" size="15"></td>
-                    <td><input type="text" name="prijs"size="3"></td>
-                    <td><input type="submit" name="toevoegenminderwerk" value="Toevoegen"></td>
-                    <td><input type="hidden" name="id" value="<?php print($_GET['id']);?>"></td>
-                </tr>
-            </table>
-        </form>
     </div>
 </div>
 <?php $pdo = NULL; ?>
