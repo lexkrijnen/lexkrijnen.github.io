9b854cdb33f384f8c3a4d0eeda072e0d552b9ba0
JeroenElferink <jeroenelferink13@gmail.com>
2017-12-14 12:45:30 +0100

    Account

--- accountoverview.php
+++ accountoverview.php
@@ -60,7 +60,7 @@
 
 
 <div class="container">
-    <div class="row page-box col-xs-4 col-xs-offset-1">
+    <div class="row page-box col-xs-4 col-xs-offset-0">
         <h1>Meer Werk</h1>
         <form method="get" action="meermindertoevoegen.php">
             <table class="table table-hover table-bordered">
