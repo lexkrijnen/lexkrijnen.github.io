a702a44b4fec4a5ec335150a9302f3535ef2ef0b
lexkrijnen <33865169+lexkrijnen@users.noreply.github.com>
2017-12-14 13:27:05 +0100

    FIX toevoegen meer/minder werk

--- Contact/verzendbericht.php
+++ Contact/verzendbericht.php
@@ -1,24 +1,75 @@
 <?php
-error_reporting(-1);
-ini_set('display_errors', 'On');
-set_error_handler("var_dump");
+/*
+ *  CONFIGURE EVERYTHING HERE
+ */
 
+// an email address that will be in the From field of the email.
+$from = 'Demo contact form <demo@domain.com>';
 
-if (isset($_POST['submit'])) {
-  $name = $_POST['naam'];
-  $subject = $_POST['onderwerp'];
-  $mailFrom = $_POST['mail'];
-  $message = $_POST['bericht'];
+// an email address that will receive the email with the output of the form
+$sendTo = 'lexkrijnen@live.nl';
 
+// subject of the email
+$subject = 'New message from contact form';
 
-  $mailTo = "markxjansen@gmail.com";
-  $headers = "From: ".$mailFrom;
-  $txt = "You have received an e-mail from ".$name.".\n\n".$message;
+// form field names and their translations.
+// array variable name => Text to appear in the email
+$fields = array('name' => 'Name', 'surname' => 'Surname', 'phone' => 'Phone', 'email' => 'Email', 'message' => 'Message');
 
-    $mail = mail($mailTo, $subject, $txt);
-    if($mail){
-        echo "Thank you for using our mail form";
-    }else{
-        echo "Mail sending failed.";
+// message that will be displayed when everything is OK :)
+$okMessage = 'Contact form successfully submitted. Thank you, I will get back to you soon!';
+
+// If something goes wrong, we will display this message.
+$errorMessage = 'There was an error while submitting the form. Please try again later';
+
+/*
+ *  LET'S DO THE SENDING
+ */
+
+// if you are not debugging and don't need error reporting, turn this off by error_reporting(0);
+error_reporting(E_ALL & ~E_NOTICE);
+
+try
+{
+
+    if(count($_POST) == 0) throw new \Exception('Form is empty');
+
+    $emailText = "You have a new message from your contact form\n=============================\n";
+
+    foreach ($_POST as $key => $value) {
+        // If the field exists in the $fields array, include it in the email
+        if (isset($fields[$key])) {
+            $emailText .= "$fields[$key]: $value\n";
+        }
     }
+
+    // All the neccessary headers for the email.
+    $headers = array('Content-Type: text/plain; charset="UTF-8";',
+        'From: ' . $from,
+        'Reply-To: ' . $from,
+        'Return-Path: ' . $from,
+    );
+
+    // Send email
+    mail($sendTo, $subject, $emailText, implode("\n", $headers));
+
+    $responseArray = array('type' => 'success', 'message' => $okMessage);
+}
+catch (\Exception $e)
+{
+    $responseArray = array('type' => 'danger', 'message' => $errorMessage);
+}
+
+
+// if requested by AJAX request return JSON response
+if (!empty($_SERVER['HTTP_X_REQUESTED_WITH']) && strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) == 'xmlhttprequest') {
+    $encoded = json_encode($responseArray);
+
+    header('Content-Type: application/json');
+
+    echo $encoded;
+}
+// else just display the message
+else {
+    echo $responseArray['message'];
 }
