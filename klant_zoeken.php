a647570cfe3ddd5d8213712e3f676949497b905d
daandejonge <daandejonge2000@live.nl>
2017-12-15 11:57:47 +0100

    knop verplaatst

--- klant_zoeken.php
+++ klant_zoeken.php
@@ -177,10 +177,10 @@ $pdo = NULL;
                         print("<tr><td>Postcode:</td><td>$postcode</td></tr>");
                         print("<tr><td>Woonplaats:</td><td>$woonplaats</td></tr>");
 												print("<form action='klant_wijzigen.php' method='get'>");
-                        print("<tr><td></td><td><input class=\"btn btn-succes\" type=\"submit\" name=\"wijzigen\" value=\"wijzigen\"></td>");
+                        print("<tr><td></td><td><input class=\"btn btn-succes\" type=\"submit\" name=\"wijzigen\" value=\"wijzigen\">");
                         print("</form>");
                         print("<form action='klant_verwijderen.php' method='get'>");
-                        print("<td><input class=\"btn btn-danger\" type=\"submit\" name=\"verwijderen\" value=\"verwijderen\"></td></tr>");
+                        print("<input class=\"btn btn-danger\" type=\"submit\" name=\"verwijderen\" value=\"verwijderen\"></td></tr>");
                         print("</form>");
                         print("</table></div>");
                     } else {
