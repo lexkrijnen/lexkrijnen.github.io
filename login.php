995fef441cef94674058ef949c0df705819e5945
lexkrijnen <33865169+lexkrijnen@users.noreply.github.com>
2017-12-15 11:30:28 +0100

    fix wanneer al ingelogd

--- login.php
+++ login.php
@@ -1,7 +1,8 @@
 <?php
 session_start();
 @$klant_id = $_SESSION['klant_id'];
-
+@$medewerker_nummer = $_SESSION['medewerker_nummer'];
+@$medewerker_functie = $_SESSION['medewerker_functie'];
 ?>
 
 <!DOCTYPE html>
@@ -56,6 +57,12 @@ session_start();
 if (!empty($klant_id)) {
     print('<div class="container page-box"><div class="col-xs-4 col-md-5"><h5>Een moment gedult A.U.B.</h5></div><br>');
     print('<meta http-equiv="refresh" content="2;url=../account.php" />');
+} else if (!empty($medewerker_nummer)) {
+    if ($medewerker_functie == "1") {
+        print('<meta http-equiv="refresh" content="0;url=../profile_admin.php" />');
+    } elseif ($medewerker_functie == "2") {
+        print('<meta http-equiv="refresh" content="0;url=../profile_medewerker.php" />');
+    }
 } else {
 ?>
 
