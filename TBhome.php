7e855663ab33b486223a164caa1df67d9c8dce3e
lexkrijnen <33865169+lexkrijnen@users.noreply.github.com>
2017-12-15 11:15:05 +0100

    fix wanneer al ingelogd

--- TBhome.php
+++ TBhome.php
@@ -1,3 +1,12 @@
+<?php
+session_start();
+@$klant_id = $_SESSION['klant_id'];
+if (!empty($klant_id)) {
+    $ingelogd = "Mijn Account";
+} else {
+    $ingelogd = "Inloggen";
+}
+?>
 
 <!DOCTYPE html>
 <html lang="en">
@@ -54,7 +63,7 @@
 						<li class="nav-item"><a href="#Vergunningen">Vergunningen</a></li>
 						<li class="nav-item"><a href="#Projecten">Projecten</a></li>
 						<li class="nav-item"><a href="#Contact">Contact</a></li>
-						<li class="nav-item"><a href="login.php">Inloggen</a></li>
+						<li class="nav-item"><a href="login.php"><?php print($ingelogd);?></a></li>
 					</ul>
 				</div><!-- /.navbar-collapse -->
 			</div><!-- /.container-fluid -->
