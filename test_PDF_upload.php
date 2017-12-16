8300e815b6465c20a96b72137927c670cc02e89b
marcprijs <33896960+marcprijs@users.noreply.github.com>
2017-12-16 15:20:31 +0100

    large modal button remove

--- test_PDF_upload.php
+++ test_PDF_upload.php
@@ -163,7 +163,6 @@
                     <div class="modal-dialog modal-lg">
                         <div class="modal-content">
                             <div class="modal-header">
-                                <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
                                 <h4 class="modal-title" id="myModalLabel">Bestanden toevoegen</h4>
                             </div>
                             <div class="modal-body">
