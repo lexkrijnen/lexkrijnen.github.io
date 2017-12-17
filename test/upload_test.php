517febeb4a70bc932489f8cff6843019c0550436
lexkrijnen <33865169+lexkrijnen@users.noreply.github.com>
2017-12-17 18:11:45 +0100

    upload probeersel

--- test/upload_test.php
+++ test/upload_test.php
@@ -4,8 +4,8 @@
 
 <form action="upload.php" method="post" enctype="multipart/form-data">
 Kies een bestand om te uploaden: <br>
-    <input type="file" name="file" id="file" size="80">
-    <input type="submit" id="u_button" value="Upload dit bestand" name="u_button">
+    <input type="file" name="file">
+    <input type="submit" name="submit" value="Upload bestand" >
 </form>
 
 </body>
