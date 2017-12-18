532b89ac97c16cb8e7273c04895e0fbabbd2fb8d
DjurnNelissen <djurn98@live.nl>
2017-12-18 13:15:53 +0100

    commented php out

--- uploadtest.php
+++ uploadtest.php
@@ -27,7 +27,7 @@
         <![endif]-->
 	</head>
   <body>
-  	<?php
+  	<?php/*
 			define("UPLOAD_DIR", "/pdf/");
 
 			if (!empty($_FILES["doc"])) {
@@ -66,7 +66,7 @@
 					if ($out[0] != $mime) {
 						echo "<p>Dit bestandsformaat is niet toegestaan.</p>";
 						exit;
-					}
+					}*/
 		?>
 
   	<nav class="navbar navbar-default" role="navigation">
