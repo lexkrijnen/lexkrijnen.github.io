d907d6b5e3dc854cdeb398919c0a73b417aaf67a
DjurnNelissen <djurn98@live.nl>
2017-12-18 12:40:39 +0100

    edited tekening

--- test_PDF_upload.php
+++ test_PDF_upload.php
@@ -154,7 +154,7 @@
             <?php
             foreach ($tekening AS $document2) {
                 print("<tr>");
-                print("<td> <a href=pdf-viewer/web/viewer.html?file=/pdf/bf-cheatingguide.pdf target= pdf_viewer>" . $document2["naam"] . "</td>");
+                print("<td> <a href='pdf-viewer/web/viewer.html?file=/pdf/" . $document2["document"] . "' target='pdf_viewer'>" . $document2["naam"] . "</td>");
                 print("</tr>");
                 }
             ?>
