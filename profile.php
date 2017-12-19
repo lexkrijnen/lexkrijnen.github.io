a63c1056651b824762ec3012b7172465b8c3e34e
marcprijs <33896960+marcprijs@users.noreply.github.com>
2017-12-19 14:33:56 +0100

    1 geheel

--- profile.php
+++ profile.php
@@ -66,10 +66,6 @@
             <!-- Collect the nav links, forms, and other content for toggling -->
             <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
                 <ul class="nav navbar-nav navbar-right">
-                    <li class="nav-item"><a href="accountoverview.php">Mijn gegevens</a></li>
-                    <li class="nav-item"><a href="meerminderlanding.php">Meer/Minder werk</a></li>
-                    <li class="nav-item"><a href="profile.php">Contract/Tekening</a></li>
-                    <li class="nav-item"><a href="account.php">Mijn Account</a></li>
                     <li class="nav-item"><a href="logout.php">Uitloggen</a></li>
                 </ul>
             </div>
