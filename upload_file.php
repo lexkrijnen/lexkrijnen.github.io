38629469f7c2b072d2f8991756213464d59323a6
DjurnNelissen <djurn98@live.nl>
2017-12-19 15:10:48 +0100

    patiphan.php

--- upload_file.php
+++ upload_file.php
@@ -1,21 +1,28 @@
 <?php
+if(isset($_FILES['file'])){
+    print_r($_FILES);
+    $errors= array();
+    $file_name = $_FILES['file']['name'];
+    $file_size =$_FILES['file']['size'];
+    $file_tmp =$_FILES['file']['tmp_name'];
+    $file_type=$_FILES['file']['type'];
+    $file_ext=strtolower(end(explode('.',$_FILES['file']['name'])));
 
- $targetfolder = "pdf/";
+    $expensions= array("pdf", "PDF");
 
- $targetfolder = $targetfolder . basename( $_FILES['file']['name']) ;
+    if(in_array($file_ext,$expensions)=== false){
+        $errors[]="extension not allowed, please choose a JPEG or PNG file.";
+    }
 
-if(move_uploaded_file($_FILES['file']['tmp_name'], $targetfolder))
+    if($file_size > 2097152){
+        $errors[]='File size must be excately 2 MB';
+    }
 
- {
-
- echo "The file ". basename( $_FILES['file']['name']). " is uploaded";
-
- }
-
- else {
-
- echo "Problem uploading file";
-
- }
-
- ?>
+    if(empty($errors)==true){
+        move_uploaded_file($file_tmp,"pdf/".$file_name);
+        echo "Success";
+    }else{
+        print_r($errors);
+    }
+}
+?>
