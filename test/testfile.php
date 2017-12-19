93cb15d7f9b7dd9d30be03dd656adf0eb8f24733
lexkrijnen <33865169+lexkrijnen@users.noreply.github.com>
2017-12-19 14:02:20 +0100

    Project Aanmaken pagina

--- test/testfile.php
+++ test/testfile.php
@@ -8,16 +8,8 @@ $pdo = new PDO($db, $user, $pass);
 
 
 //TELLEN HOEVEEL PROJECTEN ER AL BESTAAN:
-$sql1 = "SELECT max(project_nummer) FROM Project";
-$stmt1 = $pdo->prepare($sql1);
-$stmt1->execute();
-$sqlresult = $stmt1->fetch();
-
-var_dump($sqlresult);
-
-foreach ($sqlresult as $a => $b) {
-    $lastprojectnr = $b[0];
+$stmt = $pdo->query('SELECT max(project_nummer) FROM Project');
+$id = $stmt->fetchColumn(0);
+if ($id !== false) {
+    echo $id;
 }
-$projectnummer = $lastprojectnr + 1;
-
-print("Dit wordt het nieuwe projectnummer: " . $projectnummer);
