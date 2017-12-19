3cc9b54285df30e5fd57211e635549a3f8474126
DjurnNelissen <djurn98@live.nl>
2017-12-19 14:06:56 +0100

    removed br

--- test_PDF_upload.php
+++ test_PDF_upload.php
@@ -127,7 +127,6 @@
 								<div class="modal-body">
 									<form action="upload_file.php" method="post" enctype="multipart/form-data">
 										<input type="file" name="document" />
-										<br />
 										<input type="submit" value="Upload" />
 									</form>
 								</div>
