fd59e712df8ad711cfa1f86cf7671ce8ea8bc440
Mark Jansen <Markxjansen@gmail.com>
2017-12-19 12:07:35 +0100

    landing page

--- BBhome.php
+++ BBhome.php
@@ -82,7 +82,7 @@ if (!empty($klant_id OR $medewerker_nummer)) {
 		<!--<div class="">-->
 		<div class="">
 			<div class="BBhomeBackground">
-				<img src="images/Logo%20bouwbedrijf%20Wegro.png" class="logo">
+				<img src="images/LBouwbedrijf%20Wegro.png" class="logo">
 				<a href="contact.php">
 					<div href="contact.php" class="col-xs-2 col-xs-offset-5 btn select-btn contbutton">
 						Contact
