03e3b820eed4401f791cd884d114bba71669b486
lexkrijnen <33865169+lexkrijnen@users.noreply.github.com>
2017-12-20 13:47:45 +0100

    verhannesed
    Signed-off-by: lexkrijnen <33865169+lexkrijnen@users.noreply.github.com>

--- test_PDF_upload.php
+++ test_PDF_upload.php
@@ -204,7 +204,114 @@
 					<a class="btn btn-primary" onclick="window.open('pdf-viewer/web/viewer.html?file=/pdf/test.pdf', 'newwindow', 'width=600,height=1000'); return false;">Openen in nieuw scherm.</a>
 				</div>
 			</div>
-		</div>
+
+
+                <?php
+                session_start();
+                @$klant_id = $_SESSION['klant_id'];
+                @$klant_voornaam = $_SESSION['voornaam'];
+                @$medewerker_nummer = $_SESSION['medewerker_nummer'];
+
+                $db = "mysql:host=localhost; dbname=Wegro; port=3306";
+                $user = "wegro";
+                $pass = "SQLWegro@101";
+                $pdo = new PDO($db, $user, $pass);
+                //MEER WERK
+                $stmt = $pdo->prepare("SELECT * FROM Mutatie WHERE soort_nummer = 1 AND contract_nummer = :contract_nummer");
+                $stmt->execute(array(':contract_nummer' => $_GET['id']));
+                $meerwerk = $stmt->fetchAll();
+                //MINDER WERK
+                $stmt2 = $pdo->prepare("SELECT * FROM Mutatie WHERE soort_nummer = 2 AND contract_nummer = :contract_nummer");
+                $stmt2->execute(array(':contract_nummer' => $_GET['id']));
+                $minderwerk = $stmt2->fetchAll();
+                //NAAM PROJECT
+                $stmt3 = $pdo->prepare("SELECT p.naam FROM Project p JOIN Contract c ON  p.project_nummer = c.project_nummer WHERE contract_nummer = :contract_nummer");
+                $stmt3->execute(array(':contract_nummer' => $_GET['id']));
+                $naamproject = $stmt3->fetchAll();
+
+                if ($klant_id == "" AND $medewerker_nummer != ""){
+                    $rol = "medewerker";
+                } elseif($klant_id != "" AND $medewerker_nummer == ""){
+                    $rol = "klant";
+                }
+
+            if (empty($klant_id)) {
+                print('<div class="container page-box"><div class="col-xs-4 col-md-5"><h5>Sorry, u bent niet ingelogd.</h5></div><br>');
+                print('<meta http-equiv="refresh" content="2;url=../login.php" />');
+            } else {
+            ?>
+
+            <!--MEER WERK-->
+            <div class="container page-box">
+                <div class="col-xs-4">
+                    <h1>Meer Werk</h1>
+                    <?php
+                    foreach ( $naamproject as $value ) {
+                        print ("<h5>Projectnaam: " . $value['naam'] . "</h5>");
+                    }
+                    ?>
+                    <form method="get" action="meermindertoevoegen.php">
+                        <table class="table table-hover table-bordered">
+                            <tr>
+                                <th>Nr.</th>
+                                <th>Beschrijving</th>
+                                <th>Prijs</th>
+                            </tr>
+                            <?php
+                            $meerwerkcount = 1;
+                            foreach ($meerwerk AS $werk) {
+                                print("<tr>");
+                                print("<td>" . $meerwerkcount . "</td>");
+                                print("<td>" . $werk["beschrijving"] . "</td>");
+                                print("<td>€ " . $werk["prijs"] . "</td>");
+                                print("</tr>");
+                                $meerwerkcount++;
+                            }
+                            ?>
+                        </table>
+                    </form>
+                    <a href="meerminderlanding.php"><button type="button" class="btn btn-primary btn-return">Terug</button></a>
+                </div>
+
+
+                <div class="col-xs-3"></div>
+                <!-- LEGE RUIMTE TUSSEN KOLOMMEN-->
+
+                <!--MINDER WERK-->
+                <div class="col-xs-4">
+                    <h1>Minder Werk</h1>
+                    <?php
+                    foreach ( $naamproject as $value ) {
+                        print ("<h5>Projectnaam: " . $value['naam'] . "</h5>");
+                    }
+                    ?>
+                    <form method="get" action="meermindertoevoegen.php">
+                        <table class="table table-hover table-bordered">
+                            <tr>
+                                <th>Nr.</th>
+                                <th>Beschrijving</th>
+                                <th>Prijs</th>
+                            </tr>
+                            <?php
+                            $minderwerkcount = 1;
+                            foreach ($minderwerk AS $werk2) {
+                                print("<tr>");
+                                print("<td>" . $minderwerkcount . "</td>");
+                                print("<td>" . $werk2["beschrijving"] . "</td>");
+                                print("<td>- € " . $werk2["prijs"] . "</td>");
+                                print("</tr>");
+                                $minderwerkcount++;
+                            }
+                            ?>
+                        </table>
+                    </form>
+                </div>
+            </div>
+            <?php $pdo = NULL; ?>
+            <?php } ?>
+
+
+        </div>
 	</div>
 	<!-- /.container -->
 
