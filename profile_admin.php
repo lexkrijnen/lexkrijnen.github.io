7151124db5a03f8aa12c14e2d5ba80e231edf740
marcprijs <33896960+marcprijs@users.noreply.github.com>
2017-12-20 21:04:26 +0100

    functies

--- profile_admin.php
+++ profile_admin.php
@@ -61,7 +61,7 @@
                 <div class="sidebar-nav">
                     <ul class="nav">
                         <li class="active">
-                            <h4><b>Gegevens</b></h4>
+                            <h4><b>Functies</b></h4>
                         </li>
                         <li class="nav-divider"></li>
                         <li><a href="profile_admin.php">Mijn Account</a></li>
