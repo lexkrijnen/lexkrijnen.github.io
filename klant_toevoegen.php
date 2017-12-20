b3df26dadccb1abf1e83a584694820bb35736c24
daandejonge <daandejonge2000@live.nl>
2017-12-20 12:40:51 +0100

    error opgelost

--- klant_toevoegen.php
+++ klant_toevoegen.php
@@ -139,7 +139,7 @@ $hash = sha1($salt . $wachtwoord);
 								<input type="submit" class="btn oranje white" name="genereer_wachtwoord" value="Genereer">
 							</td>
 							<?php
-							if (isset[$_POST["genereer_wachtwoord"]]) {
+							if (isset($_POST["genereer_wachtwoord"])) {
 								print("<tr>");
 								alert("Let op! zorg ervoor dat u dit wachtwoord nu kopieert en ergens opslaat, het kan later niet meer gewijzigd worden.");
 								print("</tr>");
