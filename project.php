b380b7940b1a1b5dbfa6022b799c2583ea09109d
lexkrijnen <33865169+lexkrijnen@users.noreply.github.com>
2017-12-20 16:24:36 +0100

    Projecten fix3000
    Signed-off-by: lexkrijnen <33865169+lexkrijnen@users.noreply.github.com>

--- project.php
+++ project.php
@@ -30,7 +30,7 @@
     session_start();
     @$klant_id = $_SESSION['klant_id'];
     @$klant_voornaam = $_SESSION['voornaam'];
-    @$medewerker_nummer = $_SESSION['medewerker_nummer']
+    @$medewerker_nummer = $_SESSION['medewerker_nummer'];
 
     $id = $_GET['id'];
     $error = "";
@@ -45,6 +45,10 @@
     $stmt = $pdo->prepare("SELECT * FROM Contract WHERE project_nummer = '$projectid'");
     $stmt->execute();
     $contract = $stmt->fetchAll();
+
+    $stmt8 = $pdo->prepare("SELECT * FROM Project WHERE klant_nummer = '$klant_id'");
+    $stmt8->execute();
+    $projecten = $stmt8->fetchAll();
 
     //TABEL TEKENING
     if (isset($_GET["toevoegentekening"]) && isset($_GET["document"])) {
