0a8064fc3a8fc5a8a45c6f88d9f53ed4759923ce
JeroenElferink <jeroenelferink13@gmail.com>
2017-12-20 10:43:18 +0100

    mailtest

--- Contact/verzendbericht2.php
+++ Contact/verzendbericht2.php
@@ -4,7 +4,7 @@ $name = $_SESSION["mailnaam"];
 $email = "jeroen.e99@gmail.com";
 $message = $_SESSION["bericht"];
 $formcontent="From: $name \n Message: $message";
-$recipient = $_SESSION["mailadres];
+$recipient = $_SESSION["mailadres"];
 $subject = "Testmail";
 $mailheader = "From: $email \r\n";
 mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
