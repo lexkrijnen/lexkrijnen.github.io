28ef15f2b5085b1249061688718fae5c9c85cdea
lexkrijnen <33865169+lexkrijnen@users.noreply.github.com>
2017-12-20 14:43:57 +0100

    verhannesed
    Signed-off-by: lexkrijnen <33865169+lexkrijnen@users.noreply.github.com>

--- test_PDF_upload.php
+++ test_PDF_upload.php
@@ -220,13 +220,22 @@
                 $user = "wegro";
                 $pass = "SQLWegro@101";
                 $pdo = new PDO($db, $user, $pass);
+
+                $getid = $_GET['id'];
+                $getpdf = $_GET['pdf'];
+
+                //OPHALEN CONTRACTNUMMERS
+                                $stmt5 = $pdo->prepare("SELECT contract_nummer FROM Contract WHERE project_nummer = '$getid' AND document = '$getpdf'");
+                $stmt5->execute(array(':contract_nummer' => $_GET['id']));
+                $contractnummer = $stmt5->fetchAll();
+
                 //MEER WERK
-                $stmt = $pdo->prepare("SELECT * FROM Mutatie WHERE soort_nummer = 1 AND contract_nummer = :contract_nummer");
-                $stmt->execute(array(':contract_nummer' => $_GET['id']));
+                $stmt = $pdo->prepare("SELECT * FROM Mutatie WHERE soort_nummer = 1 AND contract_nummer = '$contractnummer'");
+                $stmt->execute();
                 $meerwerk = $stmt->fetchAll();
                 //MINDER WERK
-                $stmt2 = $pdo->prepare("SELECT * FROM Mutatie WHERE soort_nummer = 2 AND contract_nummer = :contract_nummer");
-                $stmt2->execute(array(':contract_nummer' => $_GET['id']));
+                $stmt2 = $pdo->prepare("SELECT * FROM Mutatie WHERE soort_nummer = 2 AND contract_nummer = '$contractnummer'");
+                $stmt2->execute();
                 $minderwerk = $stmt2->fetchAll();
                 //NAAM PROJECT
                 $stmt3 = $pdo->prepare("SELECT p.naam FROM Project p JOIN Contract c ON  p.project_nummer = c.project_nummer WHERE contract_nummer = :contract_nummer");
