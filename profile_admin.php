afd74daeeabe4b68e00b20b9a918b46d7239b1cb
marcprijs <33896960+marcprijs@users.noreply.github.com>
2017-12-20 15:19:17 +0100

    tst

--- profile_admin.php
+++ profile_admin.php
@@ -65,7 +65,7 @@
                             <h4><b>Projecten</b></h4>
                         </li>
                         <li class="nav-divider"></li>
-                        <li><a href="project_aanmaken">Project Aanmaken</a></li>
+                        <li><a href="ad_project_aanmaken.php">Project Aanmaken</a></li>
 						<li><a href="meerminderadminlanding.php">Meer/Minder Werk</a></li>
                         <li class="nav-divider"></li>
                     </ul>
