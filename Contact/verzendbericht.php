f89176c625d79c829df03eb07bc3c674959ff077
JeroenElferink <jeroenelferink13@gmail.com>
2018-01-04 19:35:35 +0100

    Testfix 2

--- Contact/verzendbericht.php
+++ Contact/verzendbericht.php
@@ -3,7 +3,7 @@ session_start();
 
 
 if (isset($_POST['submitmail'])){
-    /*if(grecaptcha && !empty(grecaptcha.getResponse())){*/
+    if(/*/grecaptcha && */!empty(grecaptcha.getResponse())){*/
         $_SESSION['captchagevuld'] = TRUE;
         $response = $_POST["g-recaptcha-response"];
         $url = 'https://www.google.com/recaptcha/api/siteverify';
@@ -37,9 +37,9 @@ if (isset($_POST['submitmail'])){
             $_SESSION['verzonden'] = TRUE;
             header('Location: ../Contact/contact.php');
         }
-    /*}elseif(grecaptcha && empty(grecaptcha.getResponse())){
+    }elseif(/*grecaptcha && */empty(grecaptcha.getResponse())){
         $_SESSION['captchagevuld'] = FALSE;
-    }*/
+    }
 }
 
 ?>
