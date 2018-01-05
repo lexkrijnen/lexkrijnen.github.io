513b99a28a63c452f804ae84fd9ed98f45eacb65
DjurnNelissen <djurn98@live.nl>
2018-01-05 14:53:45 +0100

    html indent fixed

--- MBhome.php
+++ MBhome.php
@@ -48,9 +48,9 @@
 		</div>
 
 		<div class="row">
-			<div class="col-xs-10 col-xs-offset-1">
+			<div class="col-xs-12 paragraphleft page-box">
 				<a name="Ons_bedrijf" id="Ons_bedrijf"></a>
-				<p class="col-xs-10 col-xs-offset-1 paragraphleft page-box">
+				<p>
 					<titel>Ons bedrijf</titel>
 					<br><br>
 					Wij heten u van harte welkom op de site van Metselbedrijf Wegro bv.
@@ -69,68 +69,75 @@
 		</div>
 
 		<div class="row">
-			<a name="Kwaliteiten" id="Kwaliteiten"></a>
-			<div class="col-xs-10 col-xs-offset-1  page-box">
+			<div class="col-xs-12 page-box">
+				<a name="Kwaliteiten" id="Kwaliteiten"></a>
 				<p class="paragraphleft ">
 					<titel>Kwaliteiten</titel>
-				</p><br>
+				</p>
+				<br>
+			</div>
 
-				<div class="col-lg-4 col-md-4 col-sm-6 col-xs-12">
-					<div class="hovereffect">
-						<img class="img-responsive" src="images/muur.png" alt="">
-						<div class="overlay">
-							<h1>METSELWERK</h1>
-							<p>
+			<div class="col-md-4 col-sm-6 col-xs-12">
+				<div class="hovereffect">
+					<img class="img-responsive" src="images/muur.png" alt="">
+					<div class="overlay">
+						<h1>METSELWERK</h1>
+						<p>
 
-							</p>
-						</div>
-					</div>
-				</div>
-
-				<div class="col-lg-4 col-md-4 col-sm-6 col-xs-12">
-					<div class="hovereffect">
-						<img class="img-responsive" src="images/keuken.png" alt="">
-						<div class="overlay">
-							<h1>LIJM/KITWERK</h1>
-							<p>
-
-							</p>
-						</div>
-					</div>
-				</div>
-
-				<div class="col-lg-4 col-md-4 col-sm-6 col-xs-12">
-					<div class="hovereffect">
-						<img class="img-responsive" src="images/steiger.png" alt="">
-						<div class="overlay">
-							<h1>STEIGERWERK</h1>
-							<p>
-
-
-							</p>
-						</div>
+						</p>
 					</div>
 				</div>
 			</div>
-		</div>
 
-		<a name="Metselwerk" id="Metselwerk"></a>
+			<div class="col-md-4 col-sm-6 col-xs-12">
+				<div class="hovereffect">
+					<img class="img-responsive" src="images/keuken.png" alt="">
+					<div class="overlay">
+						<h1>LIJM / KITWERK</h1>
+						<p>
+
+						</p>
+					</div>
+				</div>
+			</div>
+
+			<div class="col-md-4 col-sm-6 col-xs-12">
+				<div class="hovereffect">
+					<img class="img-responsive" src="images/steiger.png" alt="">
+					<div class="overlay">
+						<h1>STEIGERWERK</h1>
+						<p>
+
+						</p>
+					</div>
+				</div>
+			</div>
+		</div><!-- .row -->
+
 		<div class="row">
-			<p class="col-xs-10 col-xs-offset-1  paragraphright page-box">
+			<div class="col-xs-12 paragraphright page-box">
+				<a name="Metselwerk" id="Metselwerk"></a>
+				<p>
+					<titel>Metselwerk</titel>
+					<br><br>
+					Bouwbedrijf Wegro bv voert ook metsel-, lijm-, voeg-, en steigerwerk uit.
+					Door onze ruime ervaring op het gebied van metselwerk enonze vakbekwame metselaars zijn wij al geruime tijd werkzaam voor diverse grote en kleine bouwondernemers, die door ons het complete metselwerk laten uitvoeren.
+					Een belangrijk aspect is dat u door ons het gehele metselwerk laat uitvoeren, er altijd een vast aanspreekpunt op de bouw aanwezig is in de vorm van een voorman.
+					Hierdoor kan snel en adequaat worden gehandeld als er zich tijdens de bouw veranderingen doen voorkomen.
+				</p>
 				<img src="images/IMG_7453.JPG" class="col-xs-5 col-xs-offset-1 imgleft">
+			</div>
 
-				<titel>Metselwerk</titel><br><br> Bouwbedrijf Wegro bv voert ook metsel-, lijm-, voeg-, en steigerwerk uit. Door onze ruime ervaring op het gebied van metselwerk enonze vakbekwame metselaars zijn wij al geruime tijd werkzaam voor diverse grote en kleine bouwondernemers, die door ons het complete metselwerk laten uitvoeren. Een belangrijk aspect is dat u door ons het gehele metselwerk laat uitvoeren, er altijd een vast aanspreekpunt op de bouw aanwezig is in de vorm van een voorman. Hierdoor kan snel en adequaat worden gehandeld als er zich tijdens de bouw veranderingen doen voorkomen.
-			</p>
-		</div>
+			<div class="row">
+				<div class="col-xs-12 page-box">
+					<a name="Projecten" id="Projecten"></a>
+					<p class="paragraphleft ">
+						<titel>Projecten</titel>
+					</p>
+					<br>
+				</div>
 
-
-		<div class="row"><a name="Projecten" id="Projecten"></a>
-			<div class="col-xs-10 col-xs-offset-1  page-box">
-				<p class="paragraphleft ">
-					<titel>Projecten</titel>
-				</p><br>
-
-				<div class="col-lg-4 col-md-4 col-sm-6 col-xs-12">
+				<div class="col-md-4 col-sm-6 col-xs-12">
 					<div class="hovereffect">
 						<img class="img-responsive" src="images/metselmuur.png" alt="">
 						<div class="overlay">
@@ -142,7 +149,7 @@
 					</div>
 				</div>
 
-				<div class="col-lg-4 col-md-4 col-sm-6 col-xs-12">
+				<div class="col-md-4 col-sm-6 col-xs-12">
 					<div class="hovereffect">
 						<img class="img-responsive" src="images/huus.png" alt="">
 						<div class="overlay">
@@ -152,9 +159,9 @@
 							</p>
 						</div>
 					</div>
-				</div>
+				</div><
 
-				<div class="col-lg-4 col-md-4 col-sm-6 col-xs-12">
+				<div class="col-md-4 col-sm-6 col-xs-12">
 					<div class="hovereffect">
 						<img class="img-responsive" src="images/raam.png" alt="">
 						<div class="overlay">
@@ -165,55 +172,70 @@
 						</div>
 					</div>
 				</div>
+			</div><!-- .row -->
+
+			<div class="row">
+     		<div class="col-xs-12 paragraphleft page-box">
+      		<a name="Vergunningen" id="Vergunningen"></a>
+					<p>
+						<titel>Vergunningen</titel>
+						<br><br>
+						Om u als klant zo veel mogelijk werk uit handen te nemen vervaardigen wij in eigen beheer de bouwvergunningen.
+						<br><br>
+						Dit houd in dat wij zowel het tekenwerk als de benodigde berekeningen maken.
+						Ook houden wij contact met de gemeente om ervoor te zorgen dat u de bouwvergunning z.s.m. binnen heeft.
+						<br><br>
+						Met het vervaardigen van de bouwvergunning proberen wij uw eisen in combinatie met onze bouwkennis om te zetten in een prachtig ontwerp, wat als solide basis staat voor de ver/nieuwbouw.
+						<br><br>
+					</p>
+					<img src="images/IMG_2450.JPG" class="col-xs-5 col-xs-offset-1 imgright">
+				</div><!-- .col -->
+			</div><!-- .row -->
+
+			<div class="row">
+				<div class="google-maps col-xs-12">
+					<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2437.878581983825!2d5.627317716021184!3d52.336348757575266!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47c633c22a95401d%3A0xc7d3b89f5cf90a07!2sBouwbedrijf+Wegro+B.V.!5e0!3m2!1snl!2snl!4v1513166724420" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
+				</div>
 			</div>
-		</div>
-        <a name="Vergunningen" id="Vergunningen"></a>
-		<div class="row">
-			<p class="col-xs-10 col-xs-offset-1  paragraphleft page-box">
-				<img src="images/IMG_2450.JPG" class="col-xs-5 col-xs-offset-1 imgright">
 
-				<titel>Vergunningen</titel><br><br> Om u als klant zo veel mogelijk werk uit handen te nemen vervaardigen wij in eigen beheer de bouwvergunningen. <br><br> Dit houd in dat wij zowel het tekenwerk als de benodigde berekeningen maken. Ook houden wij contact met de gemeente om ervoor te zorgen dat u de bouwvergunning z.s.m. binnen heeft.<br><br> Met het vervaardigen van de bouwvergunning proberen wij uw eisen in combinatie met onze bouwkennis om te zetten in een prachtig ontwerp, wat als solide basis staat voor de ver/nieuwbouw.<br><br>
-			</p>
-		</div>
-		<div class="google-maps">
-			<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2437.878581983825!2d5.627317716021184!3d52.336348757575266!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47c633c22a95401d%3A0xc7d3b89f5cf90a07!2sBouwbedrijf+Wegro+B.V.!5e0!3m2!1snl!2snl!4v1513166724420" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
-		</div>
+			<?php footTop() ?>
 
-		<?php footTop() ?>
+			<!-- JQuery Basic Slider -->
+			<script src="js/jquery.bscslider.js"></script>
 
-		<!-- JQuery Basic Slider -->
-		<script src="js/jquery.bscslider.js"></script>
-
-		<script>
-			$('a[href^="#"]').on('click', function(event) {
-				var target = $(this.getAttribute('href'));
-				if (target.length) {
-					event.preventDefault();
-					$('html, body').stop().animate({
-						scrollTop: target.offset().top
-					}, 1000);
-				}
-			});
-
-		</script>
-		<script>
-			$(document).ready(function() {
-				$('.slider-demo').bscSlider({
-					effect: 1
+			<script>
+				$('a[href^="#"]').on('click', function(event) {
+					var target = $(this.getAttribute('href'));
+					if (target.length) {
+						event.preventDefault();
+						$('html, body').stop().animate({
+							scrollTop: target.offset().top
+						}, 1000);
+					}
 				});
-			})
 
-		</script>
-		<div id="fb-root"></div>
-		<script>
-			(function(d, s, id) {
-				var js, fjs = d.getElementsByTagName(s)[0];
-				if (d.getElementById(id)) return;
-				js = d.createElement(s);
-				js.id = id;
-				js.src = 'https://connect.facebook.net/nl_NL/sdk.js#xfbml=1&version=v2.11';
-				fjs.parentNode.insertBefore(js, fjs);
-			}(document, 'script', 'facebook-jssdk'));
-		</script>
+			</script>
 
-		<?php footBottom() ?>
+			<script>
+				$(document).ready(function() {
+					$('.slider-demo').bscSlider({
+						effect: 1
+					});
+				})
+
+			</script>
+
+			<div id="fb-root"></div>
+
+			<script>
+				(function(d, s, id) {
+					var js, fjs = d.getElementsByTagName(s)[0];
+					if (d.getElementById(id)) return;
+					js = d.createElement(s);
+					js.id = id;
+					js.src = 'https://connect.facebook.net/nl_NL/sdk.js#xfbml=1&version=v2.11';
+					fjs.parentNode.insertBefore(js, fjs);
+				}(document, 'script', 'facebook-jssdk'));
+			</script>
+
+			<?php footBottom() ?>
