b9f8780726c71e8b51136c5fd2dcad0ca408995d
DjurnNelissen <djurn98@live.nl>
2018-01-05 11:54:37 +0100

    removed md classes

--- index.php
+++ index.php
@@ -28,17 +28,17 @@
 
 			<div class="row">
 				<a href="MBhome.php">
-					<div class="col-xs-10 col-xs-offset-1 col-md-3 col-md-offset-0 btn btn-default select-btn">
+					<div class="col-xs-3 btn btn-default select-btn">
 						<img class="select-logo" src="images/MBtegel2.png" alt="logo">
 					</div>
 				</a>
 				<a href="BBhome.php">
-					<div class="col-xs-10 col-xs-offset-1 col-md-3 btn btn-default select-btn">
+					<div class="col-xs-3 col-xs-offset-1 btn btn-default select-btn">
 						<img class="select-logo" src="images/BBtegel2.png" alt="logo">
 					</div>
 				</a>
 				<a href="TBhome.php">
-					<div class="col-xs-10 col-xs-offset-1 col-md-3 btn btn-default select-btn">
+					<div class="col-xs-3 col-xs-offset-1 btn btn-default select-btn">
 						<img class="select-logo" src="images/TBtegel2.png" alt="logo">
 					</div>
 				</a>
