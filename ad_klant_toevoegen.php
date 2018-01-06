7607cb080bcd200b4fc877d0fe1c914edebd2b81
marcprijs <33896960+marcprijs@users.noreply.github.com>
2018-01-06 16:40:30 +0100

    test met geneer knop

--- ad_klant_toevoegen.php
+++ ad_klant_toevoegen.php
@@ -188,7 +188,7 @@ $hash = sha1($salt . $wachtwoord);
 									<input type="hidden" name="salt" <?php if (isset($_POST[ "genereer_wachtwoord"])) { print( "value=$salt"); } ?>>
 								</td>
 								<td>
-									<input type="submit" class="btn oranje white" name="genereer_wachtwoord" value="Genereer">
+									<input type="submit" id="button1" class="btn oranje white" name="genereer_wachtwoord" value="Genereer">
 								</td>
 							</tr>
 							<tr>
