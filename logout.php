0d7cc75e08583d8aed41141b9d629099e48a5b39
DjurnNelissen <djurn98@live.nl>
2018-01-08 07:55:05 +0100

    dwdwd

--- logout.php
+++ logout.php
@@ -20,6 +20,6 @@
     session_start();   //HAALT DE ACTIVE SESSIE OP
     session_destroy(); //LOGT DE GEBRUIKER UIT!
     print('<nav class="navbar navbar-default" role="navigation"><div class="container"><div class="navbar-header"><button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false"><span class="sr-only">Toggle navigation</span><span class="icon-bar"></span><span class="icon-bar"></span><span class="icon-bar"></span></button><a class="navbar-brand" href="../index.php"><img class="brand-logo" src="../images/wegrobanner.png" alt="logo"></a></div><div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1"></div></div></nav>');
-    print('<div class="alert alert-success container page-box"><span class="glyphicon glyphicon-ok" aria-hidden="true"></span> U bent uitgelogd.</div>');
+    print('<div class="alert alert-success container page-box col-xs-10 col-xs-offset-1 col-md-6 col-md-offset-3"><span class="glyphicon glyphicon-ok" aria-hidden="true"></span> U bent uitgelogd.</div>');
     print('<meta http-equiv="refresh" content="1;url=../index.php" />'); //AUTOMATISCHE FORWARD NAAR INDEX.PHP
 ?>
