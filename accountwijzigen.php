028d6f22cdcbbf371aa3bb20e5187f28e5f74b5e
DjurnNelissen <djurn98@live.nl>
2018-01-08 07:43:40 +0100

    dwdwd

--- accountwijzigen.php
+++ accountwijzigen.php
@@ -73,137 +73,95 @@ $projecten = $stmt->fetchAll();
 $pdo = NULL;
 ?>
 
+	<?php include 'includes.php';?>
+	<?php headTop() ?>
 
+	<title>Gegevens wijzigen</title>
 
-	<!DOCTYPE html>
-	<html lang="en">
+	<?php headMiddle() ?>
 
-	<head>
-		<meta charset="UTF-8">
-		<meta http-equiv="X-UA-Compatible" content="IE=edge">
-		<meta name="viewport" content="width=device-width, initial-scale=1">
-		<!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
-		<meta name="description" content="Welkom bij Bouwbedrijf Wegro.">
-		<meta name="author" content="Nard Wemes">
-		<link rel="icon" href="images/Logo%20bouwbedrijf%20Wegro.png">
+	<!--- custom stylesheet ophalen --->
+	<link href="css/klant_pagina.css" rel="stylesheet">
 
-		<title>wijzigen</title>
+	<?php headBottom() ?>
 
-		<!-- Bootstrap core CSS -->
-		<link href="css/bootstrap.min.css" rel="stylesheet">
+	<!-- Navbar inroepen-->
+	<?php navTop() ?>
+	<li class="nav-item"><a href="index.php">Home</a></li>
+	<li class="nav-item"><a href="Contact/contact.php">Contact</a></li>
+	<li class="nav-item"><a href="logout.php">Uitloggen</a></li>
+	<?php navBottom() ?>
 
-		<!-- Global styles for this website -->
-		<link href="css/global.css" rel="stylesheet">
-
-		<!-- Custom styles for this page -->
-		<link href="css/klant_pagina.css" rel="stylesheet">
-
-		<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-		<!--[if lt IE 9]>
-      <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
-      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-    <![endif]-->
-	</head>
-
-
-
-
-	<body>
-		<nav class="navbar navbar-default" role="navigation">
-			<div class="container">
-				<!-- Brand and toggle get grouped for better mobile display -->
-				<div class="navbar-header">
-					<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-                    <span class="sr-only">Toggle navigation</span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                </button>
-					<a class="navbar-brand" href="index.php"><img class="brand-logo" src="images/wegrobanner.png" alt="logo"></a>
-				</div>
-
-				<!-- Collect the nav links, forms, and other content for toggling -->
-				<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-					<ul class="nav navbar-nav navbar-right">
-                        <li class="nav-item"><a href="index.php">Home</a></li>
-                        <li class="nav-item"><a href="Contact/contact.php">Contact</a></li>
-                        <li class="nav-item"><a href="logout.php">Uitloggen</a></li>
-					</ul>
-				</div>
-				<!-- /.navbar-collapse -->
-			</div>
-			<!-- /.container-fluid -->
-		</nav>
-
-            <div class="container-fluid">
-        <div class="row row-offcanvas row-offcanvas-left">
-            <div class="col-xs-12 sidebar-offcanvas" id="sidebar" role="navigation">
-                <div class="sidebar-nav">
-                    <ul class="nav">
-                        <li class="active">
-                            <h4>Menu</h4>
-                        </li>
-                        <li class="nav-divider"></li>
-                        <li><a href=<?php if($rol=="klant" ){print( "account.php");}elseif($rol=="medewerker" ){print( "profile_medewerker.php");}?>>Mijn Account</a></li>
-                        <li><a href="accountoverview.php">Mijn gegevens</a></li>
-                        <li class="nav-divider"></li>
-                        <li>
-                            <h4>Mijn projecten</h4>
-                        </li>
-                        <li class="nav-divider"></li>
-                        <?php
+	<div class="container">
+		<div class="row">
+			<div class="col-xs-12" id="sidebar" role="navigation">
+				<div class="sidebar-nav">
+					<ul class="nav">
+						<li class="nav-divider"></li>
+						<li class="active">
+							<h4>Menu</h4>
+						</li>
+						<li class="nav-divider"></li>
+						<li><a href='<?php if($rol=="klant" ){print( "account.php");}elseif($rol=="medewerker" ){print( "profile_medewerker.php");}?>'>Mijn Account</a></li>
+						<li><a href="accountoverview.php">Mijn gegevens</a></li>
+						<li class="nav-divider"></li>
+						<li>
+							<h4>Mijn projecten</h4>
+						</li>
+						<li class="nav-divider"></li>
+						<?php
                         foreach ( $projecten as $value ) {
                             print ("<li><a href=\"project.php?id=" . $value['project_nummer'] . "&pdf=voorbeeld.pdf\">" . $value['naam'] . "</a></li>");
                         }
                         ?>
-                        <li class="nav-divider"></li>
-                    </ul>
-                </div>
-                <!--/.well -->
-            </div>
-            <!--/span-->
-        </div>
-    </div>
+					</ul>
+				</div>
+				<!--/.well -->
+			</div>
+			<!--/span-->
+		</div>
+	</div>
 
 
-        <div class="container page-box">
-            <div class="col-xs-12 col-md-12">
-				<h1>Wijzigen</h1>
-				<table>
-					<form action='accountwijzigen.php' method='post'>
-						<tr>
-							<td>Voornaam</td>
-							<td><input type="text" class="form-control" name="voornaam" <?php print( "value=\"$voornaam\""); ?> ></td>
-						</tr>
-						<tr>
-							<td>Tussenvoegsel</td>
-							<td><input type="text" class="form-control" name="tussenvoegsel" <?php print( "value=\"$tussenvoegsel\""); ?>></td>
-						</tr>
-						<tr>
-							<td>Achternaam</td>
-							<td><input type="text" class="form-control" name="achternaam" <?php print( "value=\"$achternaam\""); ?>></td>
-						</tr>
-						<tr>
-							<td>Telefoonnummer  </td>
-							<td><input type="text" class="form-control" name="telefoonnummer" <?php print( "value=\"$telefoonnummer\""); ?>></td>
-						</tr>
-						<tr>
-							<td>Emailadres</td>
-							<td><input type="text" class="form-control" name="emailadres" <?php print( "value=\"$emailadres\""); ?>></td>
-						</tr>
-						<tr>
-							<td>Adres</td>
-							<td><input type="text" class="form-control" name="adres" <?php print( "value=\"$adres\""); ?>></td>
-						</tr>
-						<tr>
-							<td>Postcode</td>
-							<td><input type="text" class="form-control" name="postcode" <?php print( "value=\"$postcode\""); ?>></td>
-						</tr>
-						<tr>
-							<td>Woonplaats</td>
-							<td><input type="text" class="form-control" name="woonplaats" <?php print( "value=\"$woonplaats\""); ?>></td>
-						</tr>
-						<?php
+	<div class="container">
+	<div class="row">
+		<div class="col-xs-10 col-xs-offset-1 col-md-6 col-md-offset-3 page-box">
+			<h1>Wijzigen</h1>
+			<table>
+				<form action='accountwijzigen.php' method='post'>
+					<tr>
+						<td>Voornaam</td>
+						<td><input type="text" class="form-control" name="voornaam" <?php print( "value=\"$voornaam\ ""); ?> ></td>
+					</tr>
+					<tr>
+						<td>Tussenvoegsel</td>
+						<td><input type="text" class="form-control" name="tussenvoegsel" <?php print( "value=\"$tussenvoegsel\ ""); ?>></td>
+					</tr>
+					<tr>
+						<td>Achternaam</td>
+						<td><input type="text" class="form-control" name="achternaam" <?php print( "value=\"$achternaam\ ""); ?>></td>
+					</tr>
+					<tr>
+						<td>Telefoonnummer  </td>
+						<td><input type="text" class="form-control" name="telefoonnummer" <?php print( "value=\"$telefoonnummer\ ""); ?>></td>
+					</tr>
+					<tr>
+						<td>Emailadres</td>
+						<td><input type="text" class="form-control" name="emailadres" <?php print( "value=\"$emailadres\ ""); ?>></td>
+					</tr>
+					<tr>
+						<td>Adres</td>
+						<td><input type="text" class="form-control" name="adres" <?php print( "value=\"$adres\ ""); ?>></td>
+					</tr>
+					<tr>
+						<td>Postcode</td>
+						<td><input type="text" class="form-control" name="postcode" <?php print( "value=\"$postcode\ ""); ?>></td>
+					</tr>
+					<tr>
+						<td>Woonplaats</td>
+						<td><input type="text" class="form-control" name="woonplaats" <?php print( "value=\"$woonplaats\ ""); ?>></td>
+					</tr>
+					<?php
 												if ($_SESSION["rol"] == "medewerker") {
 														print("<input type=\"hidden\" name=\"medewerkernummer\" value=$medewerker_nummer>");
 														print("<input type=\"hidden\" name=\"functie\" value=$functie>");
@@ -211,41 +169,24 @@ $pdo = NULL;
 														print("<input type=\"hidden\" name=\"klantnummer\" value=$klant_nummer>");
 												}
 											?>
-							<tr>
-								<td><a href="accountoverview.php" id="button2" class="btn btn-primary" role="button">Terug</a></td>
-								<td align='right'><input id="button1" class="btn oranje white" type="submit" name="opslaan" value="Opslaan"></td>
-							</tr>
-					</form>
-				</table>
-				<br>
+						<tr>
+							<td><a href="accountoverview.php" id="button2" class="btn btn-default select-btn btn-responsive white" role="button">Terug</a></td>
+							<td align='right'><input id="button1" class="btn btn-default select-btn btn-responsive white" type="submit" name="opslaan" value="Opslaan"></td>
+						</tr>
+				</form>
+			</table>
+			<br>
 
 
 
-				<?php
+			<?php
             if(isset($_POST["opslaan"])) {
 								print('<div class="alert alert-success"><span class="glyphicon glyphicon-ok" aria-hidden="true"></span> De wijzigingen zijn opgeslagen</div>');
             }
 
             ?>
-			</div>
 		</div>
+	</div>
 
-
-
-
-
-		<div class="row">
-			<div class="col-xs-12 text-center footer-rights">
-				<p>© Bouwbedrijf Wegro - Powered by <a href="#">Bootstrap</a> and <a href="#">Glyphicons</a>.</p>
-			</div>
-		</div>
-
-
-		<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-		<script src="js/jquery.min.js"></script>
-
-		<!-- Bootstrap Framework -->
-		<script src="js/bootstrap.min.js"></script>
-	</body>
-
-	</html>
+		<?php footAlt() ?>
+		<?php } ?>
