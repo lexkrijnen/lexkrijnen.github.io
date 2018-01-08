047705ca3f3bc3624f2d694a12557be746c58cc9
marcprijs <33896960+marcprijs@users.noreply.github.com>
2018-01-08 11:22:16 +0100

    mooi

--- admin.php
+++ admin.php
@@ -34,8 +34,8 @@
 	<div class="container">
 		<div class="row">
 			<div id="loginbox" class="mainbox col-md-6 col-md-offset-3 col-xs-12 b">
-				<div id="panel" class="panel ">
-					<div class="panel-heading oranje">
+				<div class="panel ">
+					<div id="panel" class="panel-heading oranje">
 						<div class="panel-title white">Administrator Login Wegro</div>
 
 					</div>
