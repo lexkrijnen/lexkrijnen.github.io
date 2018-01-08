452f94ba4a0a4bd76c316852c0bbfa23d99d1f5a
marcprijs <33896960+marcprijs@users.noreply.github.com>
2018-01-08 10:33:08 +0100

    opnieuw

--- accountoverview.php
+++ accountoverview.php
@@ -1,74 +1,87 @@
 <?php
-	session_start(); //sessie starten en sessievariabelen ophalen
-	@$klant_id = $_SESSION['klant_id'];
-	@$klant_voornaam = $_SESSION['voornaam'];
-	@$medewerker_nummer = $_SESSION['medewerker_nummer'];
-	@$medewerker_voornaam = $_SESSION['medewerker_voornaam'];
-	@$medewerker_functie = $_SESSION['medewerker_functie'];
+session_start();
+
+
+$db = "mysql:host=localhost; dbname=Wegro; port=3306";
+$user = "wegro";
+$pass = "SQLWegro@101";
+$pdo = new PDO($db, $user, $pass);
+
+
+if (isset($_POST["opslaan"])) {
+		if ($_SESSION["rol"] == "klant") {
+				$sql = "UPDATE Klant SET voornaam=?, tussenvoegsel=?, achternaam=?, emailadres=?, telefoon_nummer=?, adres=?, postcode=?, woonplaats=? where klant_nummer=?";
+				$stmt = $pdo->prepare($sql);
+				$stmt->execute(array($_POST["voornaam"], $_POST["tussenvoegsel"], $_POST["achternaam"], $_POST["emailadres"], $_POST["telefoonnummer"], $_POST["adres"], $_POST["postcode"], $_POST["woonplaats"], $_POST["klantnummer"]));
+		} elseif ($_SESSION["rol"] == "medewerker") {
+				$sql = "UPDATE Medewerker SET voornaam=?, tussenvoegsel=?, achternaam=?, emailadres=?, telefoon_nummer=?, adres=?, postcode=?, woonplaats=?, functie=? where medewerker_nummer=?";
+				$stmt = $pdo->prepare($sql);
+				$stmt->execute(array($_POST["voornaam"], $_POST["tussenvoegsel"], $_POST["achternaam"], $_POST["emailadres"], $_POST["telefoonnummer"], $_POST["adres"], $_POST["postcode"], $_POST["woonplaats"], $_POST["functie"], $_POST["medewerkernummer"]));
+		} else {
+                print("Error! Query is niet uitgevoerd!");
+
+        }
+
+}
 
 
 
-	//connectie met database maken
-	$db = "mysql:host=localhost; dbname=Wegro; port=3306";
-	$user = "wegro";
-	$pass = "SQLWegro@101";
-	$pdo = new PDO($db, $user, $pass);
+$voornaam = $_SESSION["voornaam"];
+$tussenvoegsel = $_SESSION["tussenvoegsel"];
+$achternaam = $_SESSION["achternaam"];
+$naam = $_SESSION["naam"];
+$telefoonnummer = $_SESSION["telefoonnummer"];
+$emailadres = $_SESSION["emailadres"];
+$adres = $_SESSION["adres"];
+$postcode = $_SESSION["postcode"];
+$woonplaats = $_SESSION["woonplaats"];
 
-	if ($klant_id != "" AND $medewerker_id == "") {//check of er een klant is ingelogd, haal vervolgens de gegevens van deze klant uit de databse op, geef tevens rol 'klant'
-			$rol = "klant";
-			$stmt = $pdo->prepare("SELECT * FROM Klant WHERE klant_nummer='$klant_id'");
-			$stmt->execute();
-			$sqlresult = $stmt->fetch();
-			$klant_nummerdb = $sqlresult["klant_nummer"];
-	}elseif ($klant_id == "" AND $medewerker_nummer != "") {//check of er een medewerker is ingelogd, haal vervolgens de gegevens van deze medewerker uit de database, geef tevens rol 'medewerker'
-			$rol = "medewerker";
-			$stmt = $pdo->prepare("SELECT * FROM Medewerker WHERE medewerker_nummer='$medewerker_nummer'");
-			$stmt->execute();
-			$sqlresult = $stmt->fetch();
-			$medewerker_nummerdb = $sqlresult["medewerker_nummer"];
-	}
+if ($_SESSION["rol"] == "klant") {
+		$klant_nummer = $_SESSION["klant_id"];
+}
 
-	$stmt = $pdo->prepare("SELECT * FROM Project WHERE klant_nummer = '$klant_id'");
-	$stmt->execute();
-	$projecten = $stmt->fetchAll();
+if ($_SESSION["rol"] == "medewerker") {
+		$functie = $_SESSION["functie"];
+		$medewerker_nummer = $_SESSION["medewerker_nummer"];
+}
 
+if(isset($_POST["opslaan"])) {
+    $voornaam = $_POST["voornaam"];
+    $tussenvoegsel = $_POST["tussenvoegsel"];
+    $achternaam = $_POST["achternaam"];
+    $naam = $_POST["naam"];
+    $telefoonnummer = $_POST["telefoonnummer"];
+    $emailadres = $_POST["emailadres"];
+    $adres = $_POST["adres"];
+    $postcode = $_POST["postcode"];
+    $woonplaats = $_POST["woonplaats"];
+		if ($_SESSION["rol"] == "klant") {
+				$klant_nummer = $_POST["klantnummer"];
+		}
+		if ($_SESSION["rol"] == "medewerker") {
+				$functie = $_POST["functie"];
+				$medewerker_nummer = $_POST["medewerkernummer"];
+		}
+}
 
-	$voornaam = ucfirst($sqlresult["voornaam"]);
-	$tussenvoegsel = $sqlresult["tussenvoegsel"];
-	$achternaam = ucfirst($sqlresult["achternaam"]);
-	$telefoonnummer = $sqlresult["telefoon_nummer"];
-	$emailadres =  $sqlresult["emailadres"];
-	$adres = $sqlresult["adres"];
-	$postcode = $sqlresult["postcode"];
-	$woonplaats = ucfirst($sqlresult["woonplaats"]);
-	$naam = $voornaam . " " . $tussenvoegsel . " " . $achternaam;
-	@$functie = $sqlresult["functie"];
+$rol = $_SESSION["rol"];
 
-	//sessievariabelen updaten
-	$_SESSION["voornaam"] = $voornaam;
-	$_SESSION["tussenvoegsel"] = $tussenvoegsel;
-	$_SESSION["achternaam"] =  $achternaam;
-	$_SESSION["naam"] = $naam;
-	$_SESSION["telefoonnummer"] = $telefoonnummer;
-	$_SESSION["emailadres"] = $emailadres;
-	$_SESSION["adres"] = $adres;
-	$_SESSION["postcode"] = $postcode;
-	$_SESSION["woonplaats"] = $woonplaats;
-	$_SESSION["medewerker_nummer"] = $medewerker_nummerdb;
-	$_SESSION["functie"] = $medewerker_functie;
+$stmt = $pdo->prepare("SELECT * FROM Project WHERE klant_nummer = '$klant_nummer'");
+$stmt->execute();
+$projecten = $stmt->fetchAll();
 
-	$pdo = NULL;
+$pdo = NULL;
 ?>
 
 	<?php include 'includes.php';?>
 	<?php headTop() ?>
 
-	<title>Accountoverview</title>
+	<title>Gegevens wijzigen</title>
 
 	<?php headMiddle() ?>
 
 	<!--- custom stylesheet ophalen --->
-	<link href="css/login.css" rel="stylesheet">
+	<link href="css/klant_pagina.css" rel="stylesheet">
 
 	<?php headBottom() ?>
 
@@ -80,41 +93,8 @@
 	<?php navBottom() ?>
 
 	<div class="container">
-
-		<!--- tabel met klant- of medewerkergegevens --->
 		<div class="row">
-			<div class="col-xs-10 col-xs-offset-1 col-md-6 col-md-offset-3 page-box">
-				<h1>Uw gegevens</h1>
-				<div class="table-responsive">
-					<table class="table table-hover table-bordered">
-						<tr>
-							<th>Veld</th>
-							<th>Gegevens</th>
-						</tr>
-						<?php
-                    if ($klant_nummerdb != "" OR $medewerker_nummerdb != "") {//checkt of klantnummer of medewerkernummer niet leeg is
-                        print("<tr><td>Naam: </td><td>$naam</td></tr>");
-                        if ($klant_nummerdb != "") {//checkt of er een klant is ingelogd zodat het klantnummer wordt weergegeven
-                            print("<tr><td>Klantnummer: </td><td>$klant_nummerdb</td></tr>");
-                        }elseif ($medewerker_nummerdb != "") {//checkt of er een medewerker is ingelogd zodat het medewerkernummer wordt weergegeven
-                            print("<tr><td>Medewerkernummer: </td><td>$medewerker_nummerdb");
-                            print("<tr><td>Rol: </td><td>$rol");
-                        }
-                        print("<tr><td>Telefoonnummer: </td><td>$telefoonnummer</td></tr>");
-                        print("<tr><td>Emailadres: </td><td>$emailadres</td></tr>");
-                        print("<tr><td>Adres: </td><td>$adres</td></tr>");
-                        print("<tr><td>Postcode: </td><td>$postcode</td></tr>");
-                        print("<tr><td>Woonplaats: </td><td>$woonplaats</td></tr>");
-                        print("</table><form action='accountwijzigen.php' method='get'><input id=\"button1\" class=\"btn btn-default select-btn white\" type=\"submit\" name=\"wijzigen\" value=\"Wijzigen\"></form></div>"); //button om accountgegevens te wijzigen
-                ?>
-					</table>
-				</div>
-			</div>
-		</div>
-
-
-		<div class="row">
-			<div class="col-xs-10 col-xs-offset-1 col-md-2 col-md-offset-0" id="sidebar" role="navigation">
+			<div class="col-xs-12" id="sidebar" role="navigation">
 				<div class="sidebar-nav">
 					<ul class="nav">
 						<li class="nav-divider"></li>
@@ -122,7 +102,7 @@
 							<h4>Menu</h4>
 						</li>
 						<li class="nav-divider"></li>
-						<li><a href='<?php if($rol=="klant" ){print( "account.php");}elseif($rol=="medewerker" ){print( "profile_medewerker.php");}//check of er een medewerker of klant is ingelogd ?>'>Mijn Account</a></li>
+						<li><a href='<?php if($rol=="klant" ){print( "account.php");}elseif($rol=="medewerker" ){print( "profile_medewerker.php");}?>'>Mijn Account</a></li>
 						<li><a href="accountoverview.php">Mijn gegevens</a></li>
 						<li class="nav-divider"></li>
 						<li>
@@ -131,7 +111,7 @@
 						<li class="nav-divider"></li>
 						<?php
                         foreach ( $projecten as $value ) {
-                            print ("<li><a href=\"project.php?id=" . $value['project_nummer'] . "&pdf=voorbeeld.pdf\">" . $value['naam'] . "</a></li>"); //print projecften van een klant
+                            print ("<li><a href=\"project.php?id=" . $value['project_nummer'] . "&pdf=voorbeeld.pdf\">" . $value['naam'] . "</a></li>");
                         }
                         ?>
 					</ul>
@@ -142,13 +122,69 @@
 		</div>
 	</div>
 
-	<?php
-    if (empty($klant_id) AND empty($medewerker_nummer)) {
-        print('<div class="container"><div class="row"><div class="col-xs-10 col-xs-offset-1 col-md-6 col-md-offset-3 page-box"><h5>Sorry, u bent niet ingelogd.</h5></div></div></div><br>'); //check of er wel is ingelogd
-        print('<meta http-equiv="refresh" content="2;url=../login.php" />');
-    }else {
-    ?>
+
+        <div class="container page-box">
+            <div class="col-xs-12 col-md-12">
+				<h1>Wijzigen</h1>
+				<table>
+					<form action='accountwijzigen.php' method='post'>
+						<tr>
+							<td>Voornaam</td>
+							<td><input type="text" class="form-control" name="voornaam" <?php print( "value=\"$voornaam\""); ?> ></td>
+						</tr>
+						<tr>
+							<td>Tussenvoegsel</td>
+							<td><input type="text" class="form-control" name="tussenvoegsel" <?php print( "value=\"$tussenvoegsel\""); ?>></td>
+						</tr>
+						<tr>
+							<td>Achternaam</td>
+							<td><input type="text" class="form-control" name="achternaam" <?php print( "value=\"$achternaam\""); ?>></td>
+						</tr>
+						<tr>
+							<td>Telefoonnummer  </td>
+							<td><input type="text" class="form-control" name="telefoonnummer" <?php print( "value=\"$telefoonnummer\""); ?>></td>
+						</tr>
+						<tr>
+							<td>Emailadres</td>
+							<td><input type="text" class="form-control" name="emailadres" <?php print( "value=\"$emailadres\""); ?>></td>
+						</tr>
+						<tr>
+							<td>Adres</td>
+							<td><input type="text" class="form-control" name="adres" <?php print( "value=\"$adres\""); ?>></td>
+						</tr>
+						<tr>
+							<td>Postcode</td>
+							<td><input type="text" class="form-control" name="postcode" <?php print( "value=\"$postcode\""); ?>></td>
+						</tr>
+						<tr>
+							<td>Woonplaats</td>
+							<td><input type="text" class="form-control" name="woonplaats" <?php print( "value=\"$woonplaats\""); ?>></td>
+						</tr>
+						<?php
+												if ($_SESSION["rol"] == "medewerker") {
+														print("<input type=\"hidden\" name=\"medewerkernummer\" value=$medewerker_nummer>");
+														print("<input type=\"hidden\" name=\"functie\" value=$functie>");
+												} elseif ($_SESSION["rol"] == "klant") {
+														print("<input type=\"hidden\" name=\"klantnummer\" value=$klant_nummer>");
+												}
+											?>
+							<tr>
+								<td><a href="accountoverview.php" id="button2" class="btn btn-primary" role="button">Terug</a></td>
+								<td align='right'><input id="button1" class="btn oranje white" type="submit" name="opslaan" value="Opslaan"></td>
+							</tr>
+					</form>
+				</table>
+				<br>
+
+
+
+				<?php
+            if(isset($_POST["opslaan"])) {
+								print('<div class="alert alert-success"><span class="glyphicon glyphicon-ok" aria-hidden="true"></span> De wijzigingen zijn opgeslagen</div>');
+            }
+
+            ?>
+			</div>
+		</div>
 
 		<?php footAlt() ?>
-		<?php } ?>
-		<?php } ?>
