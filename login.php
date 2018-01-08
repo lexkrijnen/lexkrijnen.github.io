b7dbe5b8a670a3aa552a15b1e8c1d139643d1e2b
DjurnNelissen <djurn98@live.nl>
2018-01-08 01:05:56 +0100

    fixed footer

--- login.php
+++ login.php
@@ -111,7 +111,7 @@ if (!empty($klant_id)) {
 			</div>
 			<div class="row">
 				<div class="col-xs-12 text-center footer-rights">
-					<p>Bouwbedrijf Wegro © 2009 -<script>print((new Date()).getFullYear());</script>.</p>
+					<p>Bouwbedrijf Wegro © 2009 - <script>document.write((new Date()).getFullYear());</script>.</p>
 				</div>
 			</div>
 			<script src="js/jquery.min.js"></script>
