0ad61dace40a456f0813ea5aedb6cbd82326c3a1
DjurnNelissen <djurn98@live.nl>
2018-01-08 04:12:12 +0100

    overhaul admin

--- admin.php
+++ admin.php
@@ -1,96 +1,75 @@
 <?php
-session_start();
+	session_start();
+	@$klant_id = $_SESSION['klant_id'];
+	@$medewerker_nummer = $_SESSION['medewerker_nummer'];
+	if (!empty($klant_id OR $medewerker_nummer)) {
+		$ingelogd = "Mijn Account";
+	} else {
+		$ingelogd = "Inloggen";
+	}
 ?>
 
-<!DOCTYPE html>
-<html lang="en">
+	<?php include 'includes.php';?>
+	<?php headTop() ?>
 
-<head>
-	<meta charset="UTF-8">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<meta name="description" content="Welkom bij Bouwbedrijf Wegro.">
-	<meta name="author" content="Nard Wemes">
-	<link rel="icon" href="images/Logo%20bouwbedrijf%20Wegro.png">
-	<title>Administrator Login</title>
-	<link href="css/bootstrap.min.css" rel="stylesheet">
-	<link href="css/global.css" rel="stylesheet">
+	<title>Inloggen</title>
+
+	<?php headMiddle() ?>
+
+	<!-- Custom styles for this page -->
 	<link href="css/login.css" rel="stylesheet">
+
+	<!--- reCAPTCHA loader --->
 	<script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
 	<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-    <!--- reCAPTCHA loader --->
-        <script src="https://www.google.com/recaptcha/api.js" async defer></script>
-</head>
+	<script src="https://www.google.com/recaptcha/api.js" async defer></script>
 
-<body>
+	<?php headBottom() ?>
 
-	<!-- NAV BAR -->
-	<nav class="navbar navbar-default" role="navigation">
-		<div class="container">
-			<!-- Brand and toggle get grouped for better mobile display -->
-			<div class="navbar-header">
-				<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-                <span class="sr-only">Toggle navigation</span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-            </button>
-				<a class="navbar-brand" href="index.php"><img class="brand-logo" src="images/wegrobanner.png" alt="Logo"></a>
-			</div>
-
-			<!-- Collect the nav links, forms, and other content for toggling -->
-			<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-				<ul class="nav navbar-nav navbar-right">
-					<li class="nav-item"><a href="index.php">Terug</a></li>
-				</ul>
-			</div>
-			<!-- /.navbar-collapse -->
-		</div>
-		<!-- /.container-fluid -->
-	</nav>
+	<!-- Navbar inroepen-->
+	<?php navTop() ?>
+	<li class="nav-item"><a href="index.php">Terug</a></li>
+	<?php navBottom() ?>
 
 	<div class="container">
-		<div id="loginbox" class="mainbox col-md-6 col-md-offset-3 col-sm-8 col-sm-offset-2 b ">
-			<div class="panel ">
-				<div class="panel-heading oranje">
-					<div class="panel-title white">Administrator Login Wegro</div>
+		<div class="row">
+			<div id="loginbox" class="mainbox col-md-6 col-md-offset-3 col-xs-12 b">
+				<div class="panel ">
+					<div class="panel-heading oranje">
+						<div class="panel-title white">Administrator Login Wegro</div>
 
-				</div>
-				<div class="panel-body a lowborder">
-					<form method="POST" action="../login/loginCheckAdmin.php" id="loginform" class="form-horizontal" role="form">
+					</div>
+					<div class="panel-body a lowborder">
+						<form method="POST" action="../login/loginCheckAdmin.php" id="loginform" class="form-horizontal" role="form">
 
-						<div class="input-group c">
-							<span class="input-group-addon"><i class="glyphicon glyphicon-user"></i></span>
-							<input id="login-username" type="email" class="form-control" name="emailadres" placeholder="Vul hier uw e-mailadres in">
-						</div>
-
-						<div class="input-group c">
-							<span class="input-group-addon"><i class="glyphicon glyphicon-lock"></i></span>
-							<input id="login-password" type="password" class="form-control" name="wachtwoord" placeholder="Vul hier uw wachtwoord in">
-						</div>
-
-                        <div class="g-recaptcha" data-callback="captcha_filled" data-expired-callback="captcha_expired" data-sitekey="6LeINj8UAAAAAL23qoLUp4GzzpLWgtMY5_qfG69o">
-                        </div>
-
-						<div class="form-group d">
-
-							<div class="col-sm-12 controls">
-								<input class="btn oranje white" type="submit" name="btn-login" value="Login">
+							<div class="input-group c">
+								<span class="input-group-addon"><i class="glyphicon glyphicon-user"></i></span>
+								<input id="login-username" type="email" class="form-control" name="emailadres" placeholder="Vul hier uw e-mailadres in">
 							</div>
-						</div>
-					</form>
-					<div> </div>
-				</div>
-			</div>
-		</div>
-	</div>
-	<div class="row">
-		<div class="col-xs-12 text-center footer-rights">
-			<p>© Bouwbedrijf Wegro - Powered by <a href="#">Bootstrap</a> and <a href="#">Glyphicons</a>.</p>
-		</div>
-	</div>
-	<script src="js/jquery.min.js"></script>
-	<script src="js/bootstrap.min.js"></script>
-</body>
 
-</html>
+							<div class="input-group c">
+								<span class="input-group-addon"><i class="glyphicon glyphicon-lock"></i></span>
+								<input id="login-password" type="password" class="form-control" name="wachtwoord" placeholder="Vul hier uw wachtwoord in">
+							</div>
+
+							<div class="g-recaptcha" data-callback="captcha_filled" data-expired-callback="captcha_expired" data-sitekey="6LeINj8UAAAAAL23qoLUp4GzzpLWgtMY5_qfG69o">
+							</div>
+
+							<div class="form-group">
+								<div class="col-sm-12 controls">
+									<input class="btn btn-default select-btn white d" type="submit" name="btn-login" value="Login">
+								</div>
+							</div>
+						</form>
+					</div>
+					<!-- .panel-body -->
+				</div>
+				<!-- .panel -->
+			</div>
+			<!-- .col -->
+		</div>
+		<!-- .row -->
+	</div>
+	<!-- .container -->
+
+	<?php footAlt() ?>
