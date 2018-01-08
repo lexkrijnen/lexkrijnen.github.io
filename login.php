14bb2277e0348103369a58dbac15727e2b79a062
DjurnNelissen <djurn98@live.nl>
2018-01-08 03:13:11 +0100

    beautify

--- login.php
+++ login.php
@@ -9,35 +9,35 @@
 	}
 ?>
 
-<?php include 'includes.php';?>
-<?php headTop() ?>
+	<?php include 'includes.php';?>
+	<?php headTop() ?>
 
-		<title>Inloggen</title>
+	<title>Inloggen</title>
 
-		<?php headMiddle() ?>
+	<?php headMiddle() ?>
 
-		<!-- Custom styles for this page -->
-		<link href="css/login.css" rel="stylesheet">
+	<!-- Custom styles for this page -->
+	<link href="css/login.css" rel="stylesheet">
 
-		<?php headBottom() ?>
+	<?php headBottom() ?>
 
-		<!-- Navbar inroepen-->
-		<?php navTop() ?>
-			<li class="nav-item"><a href="index.php">Home</a></li>
-			<li class="nav-item"><a href="#Ons_bedrijf">Ons Bedrijf</a></li>
-			<li class="nav-item"><a href="#Kwaliteiten">Kwaliteiten</a></li>
-			<li class="nav-item"><a href="#Metselwerk">Metselwerk</a></li>
-			<li class="nav-item"><a href="#Projecten">Projecten</a></li>
-			<li class="nav-item"><a href="#Vergunningen">Vergunningen</a></li>
-			<li class="nav-item"><a href="Contact/contact.php">Contact</a></li>
-			<li class="nav-item">
-				<a href="login.php">
-					<?php print($ingelogd);?>
-				</a>
-			</li>
-		<?php navBottom() ?>
+	<!-- Navbar inroepen-->
+	<?php navTop() ?>
+	<li class="nav-item"><a href="index.php">Home</a></li>
+	<li class="nav-item"><a href="#Ons_bedrijf">Ons Bedrijf</a></li>
+	<li class="nav-item"><a href="#Kwaliteiten">Kwaliteiten</a></li>
+	<li class="nav-item"><a href="#Metselwerk">Metselwerk</a></li>
+	<li class="nav-item"><a href="#Projecten">Projecten</a></li>
+	<li class="nav-item"><a href="#Vergunningen">Vergunningen</a></li>
+	<li class="nav-item"><a href="Contact/contact.php">Contact</a></li>
+	<li class="nav-item">
+		<a href="login.php">
+			<?php print($ingelogd);?>
+		</a>
+	</li>
+	<?php navBottom() ?>
 
-		<?php
+	<?php
 			if (!empty($klant_id)) {
 				print('<div class="container page-box"><div class="col-xs-4 col-md-5"><h5>Een moment gedult A.U.B.</h5></div><br>');
 				print('<meta http-equiv="refresh" content="0.5;url=../account.php" />');
@@ -52,39 +52,39 @@
 			} else {
 		?>
 
-			<div class="container">
-				<div id="loginbox" class="mainbox col-md-6 col-md-offset-3 col-sm-8 col-sm-offset-2 b ">
-					<div class="panel ">
-						<div class="panel-heading oranje">
-							<div class="panel-title white">Log hier in met uw Wegro account</div>
+		<div class="container">
+			<div id="loginbox" class="mainbox col-md-6 col-md-offset-3 col-sm-8 col-sm-offset-2 b ">
+				<div class="panel ">
+					<div class="panel-heading oranje">
+						<div class="panel-title white">Log hier in met uw Wegro account</div>
 
-						</div>
-						<div class="panel-body a lowborder">
-							<form method="POST" action="../login/loginCheck.php" id="loginform" class="form-horizontal" role="form">
+					</div>
+					<div class="panel-body a lowborder">
+						<form method="POST" action="../login/loginCheck.php" id="loginform" class="form-horizontal" role="form">
 
-								<div class="input-group c">
-									<span class="input-group-addon"><i class="glyphicon glyphicon-user"></i></span>
-									<input id="login-username" type="email" class="form-control" name="emailadres" placeholder="Vul hier uw e-mailadres in">
+							<div class="input-group c">
+								<span class="input-group-addon"><i class="glyphicon glyphicon-user"></i></span>
+								<input id="login-username" type="email" class="form-control" name="emailadres" placeholder="Vul hier uw e-mailadres in">
+							</div>
+
+							<div class="input-group c">
+								<span class="input-group-addon"><i class="glyphicon glyphicon-lock"></i></span>
+								<input id="login-password" type="password" class="form-control" name="wachtwoord" placeholder="Vul hier uw wachtwoord in">
+							</div>
+
+							<div class="g-recaptcha" data-callback="captcha_filled" data-expired-callback="captcha_expired" data-sitekey="6LeINj8UAAAAAL23qoLUp4GzzpLWgtMY5_qfG69o">
+							</div>
+
+							<div class="form-group d">
+
+								<div class="col-sm-12 controls">
+									<input class="btn oranje white" type="submit" name="btn-login" value="Login">
 								</div>
-
-								<div class="input-group c">
-									<span class="input-group-addon"><i class="glyphicon glyphicon-lock"></i></span>
-									<input id="login-password" type="password" class="form-control" name="wachtwoord" placeholder="Vul hier uw wachtwoord in">
-								</div>
-
-                                <div class="g-recaptcha" data-callback="captcha_filled" data-expired-callback="captcha_expired" data-sitekey="6LeINj8UAAAAAL23qoLUp4GzzpLWgtMY5_qfG69o">
-                                </div>
-
-								<div class="form-group d">
-
-									<div class="col-sm-12 controls">
-										<input class="btn oranje white" type="submit" name="btn-login" value="Login">
-									</div>
-								</div>
-							</form>
-						</div>
+							</div>
+						</form>
 					</div>
 				</div>
 			</div>
+		</div>
 
 		<?php footAlt() ?>
